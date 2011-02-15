eea1569987eae114db890a174501925070357ea8
Oleh Demyanyuk <odemyanyuk@malkosua.com>
2011-02-15 18:50:19 +0200

    Added KanBan to Agile principle

---
+++ OO_A&D/KanBan.mm
@@ -0,0 +1,8 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1292430909349" ID="ID_245784441" LINK="../Index.mm" MODIFIED="1297689878335" TEXT="KanBan">
+<node CREATED="1297689880351" ID="ID_668012743" LINK="http://agilescout.com/best-kanban-tools/" MODIFIED="1297689889249" POSITION="right" TEXT="best web tools">
+<node CREATED="1297689885266" ID="ID_933688061" MODIFIED="1297689885266" TEXT=""/>
+</node>
+</node>
+</map>
