e350eb14e67aeff421e3ebdde15628b62455b1f4
Oleh Demyanyuk <odemyanyuk@malkosua.com>
2011-02-18 19:14:05 +0200

    added MS SQL Server mind map and Java docs

---
+++ Java/Java EE.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1298040113920" ID="ID_1675668727" LINK="Java.mm" MODIFIED="1298040141461" TEXT="Java EE">
+<node CREATED="1298030625223" ID="ID_1762485794" MODIFIED="1298030627859" POSITION="left" TEXT="Servers">
+<node CREATED="1298030628695" ID="ID_1866574834" MODIFIED="1298030630521" TEXT="Jboss">
+<node CREATED="1298030630882" MODIFIED="1298032054617" TEXT="db connection">
+<node CREATED="1298030635722" LINK="http://community.jboss.org/wiki/UsingotherDatabases" MODIFIED="1298032034165" TEXT="tutor"/>
+<node CREATED="1298032054596" MODIFIED="1298032063347" TEXT="SQL SERVER 2008">
+<node CREATED="1298032044383" LINK="http://www.skill-guru.com/blog/2010/04/05/jboss-4-server-configuration-for-sql-server-2008/" MODIFIED="1298032074020" TEXT="jboss 4"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1298040156372" ID="ID_1840094102" MODIFIED="1298040159009" POSITION="right" TEXT="EJB">
+<node CREATED="1298040161715" ID="ID_582411756" MODIFIED="1298040163361" TEXT="is">
+<node CREATED="1298040163814" ID="ID_1315527117" MODIFIED="1298040169354" TEXT="Entreprise Java Beans"/>
+</node>
+</node>
+</node>
+</map>
---
+++ SQL/MS SQL.mm
@@ -0,0 +1,11 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1291299333967" ID="ID_244850022" LINK="../Index.mm" MODIFIED="1298028050401" TEXT="MS SQL">
+<node CREATED="1298028052040" ID="ID_395602777" MODIFIED="1298028069658" POSITION="right" TEXT="MS SQL Managment Studio">
+<node CREATED="1298028070287" ID="ID_1734223573" LINK="http://www.avianwaves.com/Blog/default.aspx?id=25" MODIFIED="1298028107857" TEXT="config with other port"/>
+</node>
+<node CREATED="1298028079479" ID="ID_1459948776" MODIFIED="1298028085106" POSITION="left" TEXT="Server">
+<node CREATED="1298028085369" ID="ID_1042738529" LINK="http://www.aliaspooryorik.com/blog/index.cfm/e/posts.details/post/162" MODIFIED="1298028116122" TEXT="config with other port"/>
+</node>
+</node>
+</map>
