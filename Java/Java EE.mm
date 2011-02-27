4531dcf3a94aa7c7f7cd89dd2a032ea440c883f9
Oleh Demyanyuk <olkobg@gmail.com>
2011-02-27 15:01:40 +0200

    merged with svn repo

--- Java/Java EE.mm
+++ Java/Java EE.mm
@@ -1,5 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<<<<<<< .mine
 <node COLOR="#006633" CREATED="1298040113920" ID="ID_1675668727" LINK="Java.mm" MODIFIED="1298542233086" TEXT="Java EE">
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
   <node CREATED="1298030625223" ID="ID_1762485794" MODIFIED="1298550921166" POSITION="left" TEXT="Application Servers">
@@ -39,5 +40,49 @@
       <node CREATED="1298040163814" ID="ID_1315527117" MODIFIED="1298040169354" TEXT="Entreprise Java Beans"/>
     </node>
   </node>
+=======
+<node COLOR="#006633" CREATED="1298040113920" ID="ID_1675668727" LINK="Java.mm" MODIFIED="1298542233086" TEXT="Java EE">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1298030625223" ID="ID_1762485794" MODIFIED="1298550921166" POSITION="left" TEXT="Application Servers">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node COLOR="#cc3300" CREATED="1298030628695" ID="ID_1866574834" MODIFIED="1298550966104" TEXT="Jboss">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1298550966090" ID="ID_665294606" MODIFIED="1298550970576" TEXT="config">
+<node CREATED="1298030630882" ID="ID_944192049" MODIFIED="1298032054617" TEXT="db connection">
+<node CREATED="1298030635722" ID="ID_93855176" LINK="http://community.jboss.org/wiki/UsingotherDatabases" MODIFIED="1298032034165" TEXT="tutor"/>
+<node CREATED="1298032054596" ID="ID_195938411" MODIFIED="1298032063347" TEXT="SQL SERVER 2008">
+<node CREATED="1298032044383" ID="ID_462856681" LINK="http://www.skill-guru.com/blog/2010/04/05/jboss-4-server-configuration-for-sql-server-2008/" MODIFIED="1298032074020" TEXT="jboss 4"/>
+>>>>>>> .r79
+</node>
+<<<<<<< .mine
+</map>=======
+</node>
+<node CREATED="1298307107913" ID="ID_85410144" MODIFIED="1298307112781" TEXT="ssl anable">
+<node CREATED="1298307113108" ID="ID_123484908" LINK="http://roneiv.wordpress.com/2008/01/03/jboss-tutorial-how-to-enable-ssl-https-on-jboss-as-well-as-other-nice-to-know-configurations/" MODIFIED="1298363044308" TEXT="site 1"/>
+</node>
+<node CREATED="1298363023925" ID="ID_702843245" MODIFIED="1298363026734" TEXT="jboss 5">
+<node CREATED="1298363018588" ID="ID_355165242" LINK="http://www.datadisk.co.uk/html_docs/java_app/jboss5/jboss5_securing_web_apps.htm" MODIFIED="1298363029218" TEXT="securing"/>
+</node>
+</node>
+</node>
+<node COLOR="#cc3300" CREATED="1298542236146" ID="ID_1372697038" MODIFIED="1298550887907" TEXT="Geronimo">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1298542241635" ID="ID_1300018374" MODIFIED="1298542243088" TEXT="is">
+<node CREATED="1298542243633" ID="ID_605259077" MODIFIED="1298542265323" TEXT="combination of free tools to produce best available java application server"/>
+</node>
+</node>
+<node COLOR="#cc3300" CREATED="1298550817896" ID="ID_1798575162" LINK="http://www.caucho.com/products/" MODIFIED="1298553865658" TEXT="Resin">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1298550823083" ID="ID_645051920" MODIFIED="1298550832161" TEXT="features">
+<node CREATED="1298550833604" ID="ID_1438539454" MODIFIED="1298550848829" TEXT="has php written in java"/>
+</node>
+</node>
+</node>
+<node CREATED="1298040156372" ID="ID_1840094102" MODIFIED="1298040159009" POSITION="right" TEXT="EJB">
+<node CREATED="1298040161715" ID="ID_582411756" MODIFIED="1298040163361" TEXT="is">
+<node CREATED="1298040163814" ID="ID_1315527117" MODIFIED="1298040169354" TEXT="Entreprise Java Beans"/>
+</node>
+</node>
 </node>
 </map>
+>>>>>>> .r79
