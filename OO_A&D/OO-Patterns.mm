1fb9cf27aa7bb0d0b73d71e691ef080f681c42d2
Oleh Demyanyuk <odemyanyuk@malkosua.com>
2011-03-07 16:30:08 +0200

    changed Agile, ood priciples and Patterns naming

--- OO_A&D/OO-Patterns.mm
+++ OO_A&D/OO-Patterns.mm
@@ -9,6 +9,15 @@
 </node>
 <node CREATED="1299228814289" ID="ID_858574842" MODIFIED="1299228814868" TEXT="ex"/>
 </node>
+<node CREATED="1299503893471" ID="ID_1976574325" MODIFIED="1299503925220" POSITION="right" TEXT="Decorator">
+<font BOLD="true" NAME="SansSerif" SIZE="19"/>
+<node CREATED="1299503925934" ID="ID_199136948" MODIFIED="1299503926785" TEXT="inf">
+<node CREATED="1299503927564" ID="ID_1719679507" MODIFIED="1299504221178" TEXT="used also name like wrapper"/>
+<node CREATED="1299504250029" ID="ID_951564054" MODIFIED="1299504316855" TEXT="decorator ads hi own behavior either before or/and after delegating to the object it delecetas in order to do the rest of the job"/>
+<node CREATED="1299506781125" ID="ID_187348942" MODIFIED="1299506782531" TEXT="attaches additional responsibilities to an object dynamically. Decorators provide a fl exible alternative to subclassing for extending functionality."/>
+<node CREATED="1299506786458" ID="ID_226841596" MODIFIED="1299506808273" TEXT="Java I\O the smaple system which uses decorator pattern"/>
+</node>
+</node>
 <node CREATED="1299240855354" ID="ID_312707385" MODIFIED="1299240863663" POSITION="left" TEXT="Observer">
 <font BOLD="true" NAME="SansSerif" SIZE="18"/>
 <node CREATED="1299240863883" ID="ID_5375177" MODIFIED="1299241063089" TEXT="=">
--- OO_A&D/OOD-Principles.mm
+++ OO_A&D/OOD-Principles.mm
@@ -24,12 +24,62 @@
       Encapsulate what varies.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1299242601708" ID="ID_417506604" MODIFIED="1299242602641" TEXT="Program to interfaces, not implementations."/>
 <node CREATED="1299242610645" ID="ID_851380906" MODIFIED="1299242611522" TEXT="Favor composition over inheritence."/>
 <node CREATED="1299236856920" ID="ID_1807425320" MODIFIED="1299242965187" TEXT=" Strive for loosely coupled designs between objects that interact"/>
+<node CREATED="1283930953234" ID="ID_1778766102" MODIFIED="1299498701235" TEXT="Common">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283930974984" ID="ID_1595037506" MODIFIED="1299240999287">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>DIP</b>&#160;(Depency inversion principle)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1283930987468" ID="ID_470383395" MODIFIED="1299241003193">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>OCP</b>&#160;(Open - closed principle)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1283931003859" ID="ID_439271811" MODIFIED="1299241009643">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>LSP</b>&#160;(Liskov substituion pronciple)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1299240942928" ID="ID_1565221215" MODIFIED="1299241013621">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>SRP</b>&#160;(Single responsibility principle)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
 </node>
 </node>
 </map>
