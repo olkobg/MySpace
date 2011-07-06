b953a88b77c54daece0457d34bf9bb97dc35fcea
Oleh Demyanyuk <olkobg@gmail.com>
2011-07-06 13:36:13 +0300

    added info about linux iproute and iptables info

---
+++ OS/UlOs/iptables.mm
@@ -0,0 +1,10 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1309805016619" ID="ID_703133107" LINK="Ulike.mm" MODIFIED="1309805027903" TEXT="iptables">
+<node CREATED="1309805029175" ID="ID_1587212469" MODIFIED="1309805032598" POSITION="right" TEXT="packets marking">
+<node CREATED="1309805033938" ID="ID_408871450" MODIFIED="1309805034580" TEXT="ex">
+<node COLOR="#0033ff" CREATED="1309805035282" ID="ID_1598582597" MODIFIED="1309806071133" TEXT="# iptables -t mangle -I PREROUTING -p tcp -s 10.0.0.1 --sport 25 -j MARK --set-mark 0x10"/>
+</node>
+</node>
+</node>
+</map>
