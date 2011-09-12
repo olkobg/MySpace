7ef7aa7d071d012ced1591a92ee4447fa91745c2
Oleh Demyanyuk <olkobg@gmail.com>
2011-09-13 01:24:23 +0300

    added new info fot git

--- VCSs/git.mm
+++ VCSs/git.mm
@@ -1,78 +1,90 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#990099" CREATED="1288956262924" ID="ID_641124150" LINK="../Index.mm" MODIFIED="1299014197248" TEXT="git">
+<node COLOR="#990099" CREATED="1288956262924" ID="ID_641124150" LINK="../Index.mm" MODIFIED="1311115939412" TEXT="git">
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
 <node COLOR="#0033ff" CREATED="1311115939263" ID="ID_672221752" MODIFIED="1311116077930" POSITION="right" TEXT="cmd">
 <font BOLD="true" NAME="SansSerif" SIZE="15"/>
-<node COLOR="#0033ff" CREATED="1297153951399" ID="ID_1283764578" MODIFIED="1297153953115" POSITION="right" TEXT="init">
-<node CREATED="1288956268663" ID="ID_1358117005" MODIFIED="1289985873331" TEXT="init(initializes new branch in current directory)">
-<node CREATED="1289985859904" FOLDED="true" ID="ID_1644124774" MODIFIED="1297154002580" TEXT="ex">
-<node COLOR="#0033ff" CREATED="1289985861297" ID="ID_283873597" MODIFIED="1289985866332" TEXT="git init"/>
+<node COLOR="#0033ff" CREATED="1297153951399" ID="ID_1283764578" MODIFIED="1311116543722" TEXT="init">
+<node CREATED="1288956268663" ID="ID_1358117005" MODIFIED="1289985873331" TEXT="init(initializes new branch in current directory)"/>
+<node COLOR="#0033ff" CREATED="1289985861297" ID="ID_283873597" MODIFIED="1311116055086" TEXT="git init"/>
+<node CREATED="1297153955101" MODIFIED="1297153964392" TEXT="create empty new repositorey"/>
+<node COLOR="#0033ff" CREATED="1297153967319" MODIFIED="1311116052443" TEXT="git init --bare &lt;git_rep_name&gt;.git"/>
 </node>
+<node COLOR="#0033ff" CREATED="1288956281435" ID="ID_605132199" MODIFIED="1315865596365">
+<richcontent TYPE="NODE"><html>
+						  <head>
+						    
+						  </head>
+						  <body>
+						    <p>
+						      clone
+						    </p>
+						  </body>
+						</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1311116995886" ID="ID_1201055592" MODIFIED="1311117011601" TEXT="clones git repo"/>
+<node COLOR="#0033ff" CREATED="1289985835351" ID="ID_409390105" MODIFIED="1311116567336" TEXT="git clone http://github.com/olko/MySpace.git"/>
+</node>
+<node COLOR="#0033ff" CREATED="1289405676416" ID="ID_487111464" MODIFIED="1315865596349">
+<richcontent TYPE="NODE"><html>
+						  <head></head>
+						  <body>
+						    <p>
+						      branch
+						    </p>
+						  </body>
+						</html></richcontent>
+<node CREATED="1311116426704" ID="ID_1992506106" MODIFIED="1311116529907" TEXT="work with branches (w/o params - show)"/>
+<node CREATED="1289405685400" ID="ID_282807605" MODIFIED="1289405819061" TEXT="param">
+<node COLOR="#0033ff" CREATED="1289405689541" MODIFIED="1311115954532" TEXT="-a">
+<node CREATED="1289405691591" MODIFIED="1289405696955" TEXT="look all branches"/>
+</node>
+<node COLOR="#0033ff" CREATED="1289405698199" MODIFIED="1311115954530" TEXT="-r">
+<node CREATED="1289405701012" MODIFIED="1289405707771" TEXT="look only remote branches"/>
 </node>
 <node CREATED="1297153955101" ID="ID_1443456608" MODIFIED="1297153964392" TEXT="create empty new repositorey">
-<node CREATED="1297153965077" FOLDED="true" ID="ID_810387697" MODIFIED="1297154000891" TEXT="ex">
-<node CREATED="1297153967319" ID="ID_1234403028" MODIFIED="1297153996307" TEXT="git init --bare &lt;git_rep_name&gt;.git"/>
+<node CREATED="1297153965077" FOLDED="true" ID="ID_810387697" MODIFIED="1313572830787" TEXT="ex">
+<node COLOR="#0033ff" CREATED="1297153967319" ID="ID_1234403028" MODIFIED="1313572825211" TEXT="git init --bare &lt;git_rep_name&gt;.git"/>
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1288956281435" ID="ID_805488976" MODIFIED="1299014195592" POSITION="right" TEXT="clone(copies branch)">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      clone
-    </p>
-  </body>
-</html>
-</richcontent>
+<node CREATED="1288956281435" ID="ID_805488976" MODIFIED="1299014195592" TEXT="clone(copies branch)">
 <font NAME="SansSerif" SIZE="13"/>
-<node CREATED="1311116995886" ID="ID_1201055592" MODIFIED="1311117011601" TEXT="clones git repo"/>
-<node CREATED="1289985835351" ID="ID_1121458597" MODIFIED="1311116567336" TEXT="git clone http://github.com/olko/MySpace.git"/>
+<node CREATED="1289381984931" FOLDED="true" ID="ID_257585465" MODIFIED="1313572831435" TEXT="ex">
+<node COLOR="#0033ff" CREATED="1289985835351" ID="ID_1121458597" MODIFIED="1313572823115" TEXT="git clone &lt;git url&gt;"/>
 </node>
 </node>
-<node CREATED="1289405676416" ID="ID_564289145" MODIFIED="1289985763503" POSITION="right" TEXT="branch(see branches)">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      branch
-    </p>
-  </body>
-</html>
-</richcontent>
-<node CREATED="1311116426704" ID="ID_1992506106" MODIFIED="1311116529907" TEXT="work with branches (w/o params - show)"/>
-<node CREATED="1289405685400" ID="ID_598186148" MODIFIED="1289405819061" TEXT="param">
-<node CREATED="1289405689541" ID="ID_797506060" MODIFIED="1289405691309" TEXT="-a">
+<node CREATED="1289405676416" ID="ID_564289145" MODIFIED="1289985763503" TEXT="branch(see branches)">
+<node CREATED="1289405685400" FOLDED="true" ID="ID_598186148" MODIFIED="1313572838042" TEXT="param">
+<node COLOR="#0033ff" CREATED="1289405689541" ID="ID_797506060" MODIFIED="1313572820045" TEXT="-a">
 <node CREATED="1289405691591" ID="ID_1942603325" MODIFIED="1289405696955" TEXT="look all branches"/>
 </node>
-<node CREATED="1289405698199" ID="ID_1628645879" MODIFIED="1289405700375" TEXT="-r">
+<node COLOR="#0033ff" CREATED="1289405698199" ID="ID_1628645879" MODIFIED="1313572820045" TEXT="-r">
 <node CREATED="1289405701012" ID="ID_35689350" MODIFIED="1289405707771" TEXT="look only remote branches"/>
 </node>
-<node CREATED="1289405746631" ID="ID_1467794210" MODIFIED="1289405749707" TEXT="-d">
+<node COLOR="#0033ff" CREATED="1289405746631" MODIFIED="1313572820044" TEXT="-d">
+<node CREATED="1289405750210" MODIFIED="1289405755295" TEXT="delete branch"/>
+<node COLOR="#0033ff" CREATED="1289405746631" ID="ID_1467794210" MODIFIED="1311115954529" TEXT="-d">
 <node CREATED="1289405750210" ID="ID_775910750" MODIFIED="1289405755295" TEXT="delete branch"/>
 </node>
 </node>
 </node>
-<node CREATED="1289405808665" ID="ID_1232653318" MODIFIED="1289985763133" POSITION="right" TEXT="checkout">
-<node CREATED="1289405811667" ID="ID_1826829453" MODIFIED="1289405815511" TEXT="param">
-<node CREATED="1289405820303" ID="ID_983752232" MODIFIED="1311115973556">
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1289405808665" ID="ID_616427300" MODIFIED="1311115950592" TEXT="checkout">
+<node CREATED="1289405811667" ID="ID_613517407" MODIFIED="1289405815511" TEXT="param">
+<node CREATED="1289405820303" ID="ID_594507726" MODIFIED="1311115973556">
 <richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font color="#0000ff">-b</font>&#160;&lt;name&gt;
-    </p>
-  </body>
-</html>
-</richcontent>
-<node CREATED="1289405830696" ID="ID_549342279" MODIFIED="1311116470262" TEXT="crates new branch from given one"/>
+									  <head>
+									    
+									  </head>
+									  <body>
+									    <p>
+									      <font color="#0000ff">-b</font>&#160;&lt;name&gt;
+									    </p>
+									  </body>
+									</html></richcontent>
+<node CREATED="1289405830696" ID="ID_179972832" MODIFIED="1311116470262" TEXT="crates new branch from given one"/>
 </node>
 </node>
 </node>
@@ -115,11 +127,28 @@
 <node COLOR="#0033ff" CREATED="1311117648174" ID="ID_1934480221" MODIFIED="1311117652507" TEXT="git log --graph --pretty=oneline --abbrev-commit"/>
 </node>
 </node>
+<node COLOR="#0033ff" CREATED="1313572779285" ID="ID_896395638" MODIFIED="1315865780522" TEXT="diff">
+<node CREATED="1313572784545" ID="ID_1844496790" MODIFIED="1315865688401" TEXT="view current staged changes">
+<node COLOR="#0033ff" CREATED="1313572795090" ID="ID_631869903" MODIFIED="1313572817188" TEXT="git diff --cached"/>
 </node>
-<node CREATED="1289412005624" ID="ID_1466793587" MODIFIED="1297153935509" POSITION="left" TEXT="config">
+<node CREATED="1313572802694" ID="ID_799495547" MODIFIED="1313572806721" TEXT="view with color">
+<node COLOR="#0033ff" CREATED="1313572807046" ID="ID_1802370194" MODIFIED="1313572815709" TEXT="git diff --color"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1289412005624" ID="ID_1466793587" MODIFIED="1315865780518" TEXT="config">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1289412008534" ID="ID_793420099" MODIFIED="1289412013268" TEXT="change merge tool">
 <node CREATED="1289412013925" ID="ID_1715652734" MODIFIED="1289412020108" TEXT="git config merge.tool &lt;tool&gt;"/>
+</node>
+</node>
+</node>
+<node COLOR="#ff6666" CREATED="1311116090530" ID="ID_1081305118" MODIFIED="1311117546574" POSITION="left" TEXT="Tips">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1311116129871" ID="ID_538447464" MODIFIED="1311116135869" TEXT="fix svn rebase">
+<node CREATED="1311116138985" ID="ID_1672421427" LINK="http://brandon.dimcheff.com/2009/01/04/commit-a-linear-git-history-to-subversion.html" MODIFIED="1311116151127" TEXT="site"/>
+<node COLOR="#990099" CREATED="1311116180429" ID="ID_1646275026" LINK="http://stackoverflow.com/questions/457694/how-to-commit-a-git-repo-to-an-empty-repo-svn-server/981765#981765" MODIFIED="1311116216344" TEXT="SO">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
 </node>
 </node>
 <node CREATED="1297154031869" ID="ID_408163925" MODIFIED="1311116238376" POSITION="left" TEXT="repositor">
@@ -143,15 +172,5 @@
 </node>
 </node>
 </node>
-<node COLOR="#ff6666" CREATED="1311116090530" ID="ID_1081305118" MODIFIED="1311117546574" POSITION="left" TEXT="Tips">
-<font BOLD="true" NAME="SansSerif" SIZE="15"/>
-<node CREATED="1311116129871" ID="ID_538447464" MODIFIED="1311116135869" TEXT="fix svn rebase">
-<node CREATED="1311116138985" ID="ID_1672421427" LINK="http://brandon.dimcheff.com/2009/01/04/commit-a-linear-git-history-to-subversion.html" MODIFIED="1311116151127" TEXT="site"/>
-<node COLOR="#990099" CREATED="1311116180429" ID="ID_1646275026" LINK="http://stackoverflow.com/questions/457694/how-to-commit-a-git-repo-to-an-empty-repo-svn-server/981765#981765" MODIFIED="1311116216344" TEXT="SO">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-</node>
-</node>
 </node>
 </map>
-<!-- </node>
-</map> -->
