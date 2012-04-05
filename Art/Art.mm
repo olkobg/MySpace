f64974893c3ecda1012c7bdd62dab21b29eea36e
Oleh Demyanyuk <olkobg@gmail.com>
2012-04-05 20:51:02 +0300

    added guitar tuning

--- Art/Art.mm
+++ Art/Art.mm
@@ -4,7 +4,121 @@
 <edge COLOR="#ff00d9"/>
 <font BOLD="true" NAME="SansSerif" SIZE="17"/>
 <node CREATED="1304581287284" ID="ID_1082414805" MODIFIED="1304583573866" POSITION="right" TEXT="color themes">
-<node CREATED="1304581299458" ID="ID_656059187" MODIFIED="1304581306449" TEXT="adobe kuler"/>
+<node CREATED="1304581299458" ID="ID_656059187" LINK="http://kuler.adobe.com/" MODIFIED="1333616406145" TEXT="adobe kuler"/>
+</node>
+<node CREATED="1333616200356" ID="ID_1885882506" MODIFIED="1333616265720" POSITION="left" TEXT="Guitar">
+<node CREATED="1333616208711" ID="ID_865858686" MODIFIED="1333616266459" TEXT="tuning">
+<node CREATED="1333616228984" ID="ID_378546273" MODIFIED="1333616255612">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <table style="margin-bottom: 0; font-family: sans-serif; margin-top: 0; line-height: 20px; word-spacing: 0px; border-left-width: 1px; font-weight: normal; font-size: 13px; border-bottom-width: 1px; font-variant: normal; color: rgb(0, 0, 0); border-right-width: 1px; border-top-width: 1px; font-style: normal; letter-spacing: normal; text-transform: none; text-indent: 0px; margin-left: 0px; white-space: normal; text-align: -webkit-auto; margin-right: 0; background-color: rgb(249, 249, 249)" http-equiv="content-type" content="text/html; charset=utf-8" class="wikitable">
+      <tr>
+        <th style="text-align: center; border-top-width: 1px; background-color: rgb(242, 242, 242); padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          String
+        </th>
+        <th style="text-align: center; border-top-width: 1px; background-color: rgb(242, 242, 242); padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          Note
+        </th>
+        <th style="text-align: center; border-top-width: 1px; background-color: rgb(242, 242, 242); padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <font color="rgb(11, 0, 128)"><a style="background-image: none; background-position: initial initial; background-repeat: repeat; text-decoration: none; color: rgb(11, 0, 128); background-attachment: scroll" href="http://en.wikipedia.org/wiki/Frequency" title="Frequency">Frequency</a></font>
+        </th>
+        <th style="text-align: center; border-top-width: 1px; background-color: rgb(242, 242, 242); padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <font color="rgb(11, 0, 128)"><a style="background-image: none; background-position: initial initial; background-repeat: repeat; text-decoration: none; color: rgb(11, 0, 128); background-attachment: scroll" href="http://en.wikipedia.org/wiki/Scientific_pitch_notation" title="Scientific pitch notation">Scientific pitch notation</a></font>
+        </th>
+      </tr>
+      <tr>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          1 (Highest)
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <b>e'</b>
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          329.63&#160;Hz
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          E4
+        </td>
+      </tr>
+      <tr>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          2
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <b>b</b>
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          246.94&#160;Hz
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          B3
+        </td>
+      </tr>
+      <tr>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          3
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <b>g</b>
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          196.00&#160;Hz
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          G3
+        </td>
+      </tr>
+      <tr>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          4
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <b>d</b>
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          146.83&#160;Hz
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          D3
+        </td>
+      </tr>
+      <tr>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          5
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <b>A</b>
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          110.00&#160;Hz
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          A2
+        </td>
+      </tr>
+      <tr>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          6 (Lowest)
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          <b>E</b>
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          &#160;82.41&#160;Hz
+        </td>
+        <td style="border-top-width: 1px; padding-left: 0; padding-bottom: 0; border-left-width: 1px; padding-top: 0; border-bottom-width: 1px; padding-right: 0; border-right-width: 1px">
+          E2
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </map>
