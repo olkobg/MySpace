89906ff113c5b94f0a5e33cfaaf7df166d3bd45a
Oleh Demyanyuk <olkobg@gmail.com>
2012-04-12 20:34:18 +0300

    Added configuration of Apache with mpm-worker and fastCGI and php5

---
+++ OS/Cross/Http/Apache.mm
@@ -0,0 +1,13 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1334251753207" ID="ID_140688017" LINK="../../Crossplatform.mm" MODIFIED="1334251811259" TEXT="Apache">
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#990099" CREATED="1334251787597" ID="ID_1202095731" MODIFIED="1334251826313" POSITION="right" TEXT="configure ">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1334251794329" ID="ID_965286396" LINK="http://ubuntuforums.org/showthread.php?t=1038416" MODIFIED="1334251832014" TEXT="mpm-Worker + php5">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="penguin"/>
+</node>
+</node>
+</node>
+</map>
