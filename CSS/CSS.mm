2c295a25cf5f3209a9e0c0d8fa13284121e16b07
Oleh Demyanyuk <olkobg@gmail.com>
2012-05-13 23:16:28 +0300

    Added ddsmenu site and border-radius css3 small info

--- CSS/CSS.mm
+++ CSS/CSS.mm
@@ -17,11 +17,33 @@
       <b style="font-family: Times New Roman; text-align: -webkit-auto; font-style: normal; color: rgb(0, 0, 0); line-height: normal; word-spacing: 0px; font-weight: normal; text-indent: 0px; text-transform: none; white-space: normal; letter-spacing: normal; font-size: medium; font-variant: normal" id="internal-source-marker_0.0904492421541363"><font face="Times New Roman" size="medium" color="rgb(0, 0, 0)">/* Fallback for web browsers that doesn't support RGBa */</font></b><font face="Times New Roman" size="medium" color="#0000ff"><b style="text-align: -webkit-auto; font-family: Times New Roman; font-style: normal; color: rgb(0, 0, 0); line-height: normal; word-spacing: 0px; text-indent: 0px; font-weight: normal; text-transform: none; white-space: normal; letter-spacing: normal; font-size: medium; font-variant: normal" id="internal-source-marker_0.0904492421541363"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></b></font><font face="Consolas" size="3" color="#0000ff"><b style="text-align: -webkit-auto; font-family: Times New Roman; font-style: normal; color: rgb(0, 0, 0); line-height: normal; word-spacing: 0px; text-indent: 0px; font-weight: normal; text-transform: none; white-space: normal; letter-spacing: normal; font-size: medium; font-variant: normal" id="internal-source-marker_0.0904492421541363">background: rgb(0, 0, 0);</b></font><b style="text-align: -webkit-auto; font-family: Times New Roman; font-style: normal; color: rgb(0, 0, 0); line-height: normal; word-spacing: 0px; text-indent: 0px; font-weight: normal; text-transform: none; white-space: normal; letter-spacing: normal; font-size: medium; font-variant: normal" id="internal-source-marker_0.0904492421541363"><font face="Times New Roman" size="3" color="#0000ff"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></font><font face="Consolas" size="3" color="#0000ff">/* RGBa with 0.6 opacity */</font><font face="Times New Roman" size="3" color="#0000ff"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></font><font face="Consolas" size="3" color="#0000ff">background: rgba(0, 0, 0, 0.6);</font><font face="Times New Roman" size="3" color="#0000ff"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></font><font face="Consolas" size="3" color="#0000ff">/* For IE 5.5 - 7*/</font><font face="Times New Roman" size="3" color="#0000ff"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></font><font face="Consolas" size="3" color="#0000ff">filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);</font><font face="Times New Roman" size="3" color="#0000ff"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></font><font face="Consolas" size="3" color="#0000ff">./* For IE 8*/</font><font face="Times New Roman" size="3" color="#0000ff"><br align="-webkit-auto" size="3" style="line-height: normal; word-spacing: 0px; text-indent: 0px; white-space: normal; text-transform: none; letter-spacing: normal; font-variant: normal" color="#0000ff" /></font><font face="Consolas" size="3" color="#0000ff">-ms-filter: &quot;progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)&quot;;</font></b>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="13"/>
 </node>
 </node>
+</node>
+<node CREATED="1336845362558" ID="ID_1421273977" MODIFIED="1336845364391" POSITION="left" TEXT="border">
+<node CREATED="1336845365448" ID="ID_1780697231" LINK="http://www.css3.info/preview/rounded-border/" MODIFIED="1336845486955" TEXT="radius">
+<node COLOR="#0033ff" CREATED="1336845502117" ID="ID_84836676" MODIFIED="1336845526236">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      border-radius
+    </p>
+    <p>
+      -moz-border-radius
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1336940083970" ID="ID_657813283" MODIFIED="1336940098799" POSITION="right" TEXT="Tricks">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1336940086438" ID="ID_439660483" LINK="http://www.dynamicdrive.com/dynamicindex1/ddsmoothmenu.htm" MODIFIED="1336940094646" TEXT="ddsmenu"/>
 </node>
 </node>
 </map>
