4f1f8a666f89b776b452a2f68433cd8741cadfd5
Oleh Demyanyuk <olkobg@gmail.com>
2012-05-29 01:09:39 +0300

    git added info about css shadow and box radius

--- CSS/CSS.mm
+++ CSS/CSS.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1298379398131" ID="ID_1454701070" LINK="../Index.mm" MODIFIED="1329167912975" TEXT="CSS">
+<node CREATED="1298379398131" ID="ID_1454701070" LINK="../Index.mm" MODIFIED="1338239998359" TEXT="CSS">
 <font BOLD="true" NAME="SansSerif" SIZE="17"/>
 <node CREATED="1329167857520" ID="ID_1738733773" MODIFIED="1329167859684" POSITION="right" TEXT="Fonts">
 <node CREATED="1329167860573" ID="ID_1617167628" LINK="http://www.webfonts.info/wiki/index.php?title=%40font-face_support_in_Firefox" MODIFIED="1329167893664" TEXT="Web Fonts - Firefox"/>
@@ -22,7 +22,8 @@
 </node>
 </node>
 </node>
-<node CREATED="1336845362558" ID="ID_1421273977" MODIFIED="1336845364391" POSITION="left" TEXT="border">
+<node CREATED="1338239998207" ID="ID_766402262" MODIFIED="1338242886293" POSITION="left" TEXT="box">
+<node CREATED="1336845362558" ID="ID_1421273977" MODIFIED="1336845364391" TEXT="border">
 <node CREATED="1336845365448" ID="ID_1780697231" LINK="http://www.css3.info/preview/rounded-border/" MODIFIED="1336845486955" TEXT="radius">
 <node COLOR="#0033ff" CREATED="1336845502117" ID="ID_84836676" MODIFIED="1336845526236">
 <richcontent TYPE="NODE"><html>
@@ -38,12 +39,89 @@
     </p>
   </body>
 </html></richcontent>
+<node COLOR="#338800" CREATED="1338240262012" ID="ID_1403441543" MODIFIED="1338240273293">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -moz-border-radius-bottomright: 50px 25px;<br />border-bottom-right-radius: 50px 25px;
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1336940083970" ID="ID_657813283" MODIFIED="1336940098799" POSITION="right" TEXT="Tricks">
+</node>
+<node CREATED="1338239993612" ID="ID_781897182" MODIFIED="1338242888867" TEXT="shadow">
+<node CREATED="1338240003013" ID="ID_1096732930" LINK="http://css-tricks.com/snippets/css/css-box-shadow/" MODIFIED="1338242889580" TEXT="site1">
+<node COLOR="#0033ff" CREATED="1338240386255" ID="ID_813752905" MODIFIED="1338240456422">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      .shadow {
+    </p>
+    <p>
+      &#160;&#160;-moz-box-shadow:&#160;&#160;&#160;&#160;3px 3px 5px 6px #ccc;
+    </p>
+    <p>
+      &#160;&#160;-webkit-box-shadow: 3px 3px 5px 6px #ccc;
+    </p>
+    <p>
+      &#160;&#160;box-shadow:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;3px 3px 5px 6px #ccc;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1338240047506" ID="ID_1108520539" MODIFIED="1338240378788">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        The horizontal offset&#160;of the shadow, positive means the shadow will be on the right of the box, a negative offset will put the shadow on the left of the box.
+      </li>
+      <li>
+        The vertical offset&#160;of the shadow, a negative one means the box-shadow will be above the box, a positive one means the shadow will be below the box.
+      </li>
+      <li>
+        The blur radius&#160;(optional), if set to 0 the shadow will be sharp, the higher the number, the more blurred it will be.
+      </li>
+      <li>
+        The spread radius&#160;(optional), positive values increase the size of the shadow, negative values decrease the size. Default is 0 (the shadow is same size as blur).
+      </li>
+      <li>
+        Color
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1338240016539" ID="ID_1015713349" LINK="http://robertnyman.com/2010/03/16/drop-shadow-with-css-for-all-web-browsers/" MODIFIED="1338240033121" TEXT="site2"/>
+</node>
+</node>
+<node CREATED="1336940083970" ID="ID_657813283" MODIFIED="1337895069988" POSITION="right" TEXT="Tricks">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1337895069867" ID="ID_437743830" MODIFIED="1337895071546" TEXT="menu">
+<node CREATED="1337895075588" ID="ID_494470881" MODIFIED="1337895081818" TEXT="pipe separators">
+<node CREATED="1337895083794" ID="ID_418273686" LINK="http://css.maxdesign.com.au/listamatic/horizontal32.htm" MODIFIED="1337895097456" TEXT="site1"/>
+<node CREATED="1337895209047" ID="ID_1430090062" LINK="http://stackoverflow.com/questions/567988/nav-menu-rendering-dividing-pipe-characters" MODIFIED="1337895216527" TEXT="so"/>
+</node>
 <node CREATED="1336940086438" ID="ID_439660483" LINK="http://www.dynamicdrive.com/dynamicindex1/ddsmoothmenu.htm" MODIFIED="1336940094646" TEXT="ddsmenu"/>
+</node>
 </node>
 </node>
 </map>
