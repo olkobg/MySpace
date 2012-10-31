b2532a3f6b42756e36155057dbbc057a569741a2
Oleh Demyanyuk <olkobg@gmail.com>
2012-10-31 18:13:33 +0200

    Changed vim tab info

--- OS/Cross/vim.mm
+++ OS/Cross/vim.mm
@@ -35,14 +35,11 @@
 </node>
 </node>
 <node CREATED="1310044236747" ID="ID_424960842" MODIFIED="1310470147292" POSITION="right" TEXT="Tabulation">
-<node CREATED="1310044244954" ID="ID_853659422" MODIFIED="1310044404262" TEXT="inf">
 <node CREATED="1310044404621" ID="ID_1292241249" MODIFIED="1310044405925" TEXT="tabstop Set tabstop to tell vim how many columns a tab counts for. Linux kernel code expects each tab to be eight columns wide. Visual Studio expects each tab to be four columns wide. This is the only command here that will affect how existing text displays.  expandtab When expandtab is set, hitting Tab in insert mode will produce the appropriate number of spaces.  shiftwidth Set shiftwidth to control how many columns text is indented with the reindent operations (&lt;&lt; and &gt;&gt;) and automatic C-style indentation.  softtabstop Set softtabstop to control how many columns vim uses when you hit Tab in insert mode. If softtabstop is less than tabstop and expandtab is not set, vim will use a combination of tabs and spaces to make up the desired spacing. If softtabstop equals tabstop and expandtab is not set, vim will always use tabs. When expandtab is set, vim will always use the appropriate number of spaces.  "/>
-</node>
-<node CREATED="1310044407140" ID="ID_304392494" MODIFIED="1310044409315" TEXT="ex">
 <node COLOR="#0033ff" CREATED="1310044409676" ID="ID_1676944467" MODIFIED="1310045250150" TEXT=":set tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab"/>
 <node COLOR="#0033ff" CREATED="1310044415971" ID="ID_1009113236" MODIFIED="1310045250149" TEXT=":set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab"/>
 <node COLOR="#0033ff" CREATED="1310044425146" ID="ID_1734211713" MODIFIED="1310045250148" TEXT=":set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab "/>
-</node>
+<node COLOR="#0033ff" CREATED="1351699979211" ID="ID_1598390074" MODIFIED="1351699984900" TEXT=":set ts=2"/>
 </node>
 <node CREATED="1310469536627" ID="ID_1384056861" MODIFIED="1310469558583" POSITION="right" TEXT="Whitespaces">
 <node CREATED="1310469558569" ID="ID_1083347438" MODIFIED="1310469561224" TEXT="show">
