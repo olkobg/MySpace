9e61773b75c2429fe6130422400735cd1b20658d
Oleh Demyanyuk <olkobg@gmail.com>
2012-11-12 19:30:58 +0200

    -Added info about sublime Package manager

--- OS/Cross/Sublime.mm
+++ OS/Cross/Sublime.mm
@@ -9,6 +9,13 @@
 <node CREATED="1348054452693" ID="ID_869312784" LINK="http://stackoverflow.com/questions/11290606/sublime-text-2-copy-file-on-save-backup-to-another-folder" MODIFIED="1348054463234" TEXT="stackO"/>
 <node CREATED="1348054426028" ID="ID_1158431672" LINK="http://www.cuvedev.net/2011/12/sublime-text-2-rsync-on-save/" MODIFIED="1348054491816" TEXT="simple rsync"/>
 </node>
+<node CREATED="1352715895899" ID="ID_1829880842" LINK="http://wbond.net/sublime_packages/package_control" MODIFIED="1352715968498" TEXT="Plugin Controll"/>
+</node>
+<node CREATED="1352716585132" ID="ID_1303016478" MODIFIED="1352716602808" POSITION="left" TEXT="build system">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1352716591221" ID="ID_1252305111" LINK="http://joeybaker.tumblr.com/post/7304011987/how-to-add-php-validation-to-sublime-text-2" MODIFIED="1352716620031" TEXT="validate php">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
 </node>
 </node>
 </map>
