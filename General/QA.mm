102e3a093d314481269dfe3124cfd7ad02901266
Oleh Demyanyuk <olkobg@gmail.com>
2013-02-04 14:12:57 +0200

    -Added qa info -Added Calibre info

---
+++ General/QA.mm
@@ -0,0 +1,7 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#006699" CREATED="1356437366236" ID="ID_1690554490" LINK="../Index.mm" MODIFIED="1356437406003" TEXT="QA">
+<font BOLD="true" NAME="SansSerif" SIZE="17"/>
+<node CREATED="1356437553324" ID="ID_1638441621" LINK="http://istqb.org/" MODIFIED="1356437577574" POSITION="right" TEXT="ISTQB"/>
+</node>
+</map>
---
+++ OS/Cross/fun/Calibre.mm
@@ -0,0 +1,42 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1359978122539" ID="ID_716088760" LINK="../../Crossplatform.mm" MODIFIED="1359978265242" TEXT="Calibre">
+<node CREATED="1359978265215" ID="ID_1596013262" MODIFIED="1359978269899" POSITION="right" TEXT="headless sever">
+<node CREATED="1359978157699" ID="ID_668374268" LINK="http://linuxphilia.blogspot.com/2012/06/my-wife-and-i-like-to-read-books-on-go.html" MODIFIED="1359978310648">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#009999"><b>linuxphlia </b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#009999" CREATED="1359978295841" ID="ID_899598479" LINK="http://b42.cz/notes/calibre_server/" MODIFIED="1359978316050" TEXT="b42.cz">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1359978502158" ID="ID_355339753" LINK="http://alt236.blogspot.com/2010/09/gettng-calibre-server-to-run-at-boot-in.html" MODIFIED="1359978539938">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#009999"><b>alt236</b></font>&#160;on <b>Ubuntu</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1359978605075" ID="ID_1591175111" MODIFIED="1359978606782" POSITION="left" TEXT="manual">
+<node CREATED="1359978607843" ID="ID_637753981" LINK="http://manual.calibre-ebook.com/server.html" MODIFIED="1359978616660" TEXT="ijntegration with other servers"/>
+<node COLOR="#0033ff" CREATED="1359978617479" ID="ID_13916666" LINK="http://manual.calibre-ebook.com/cli/calibre-server.html" MODIFIED="1359978641808" TEXT="calibre-server">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
