f72b9d49212dfaf51cca7e2e2881e1df190c740f
Oleh Demyanyuk <olkobg@gmail.com>
2013-03-01 18:57:19 +0200

    -Moved Selenium to other map

---
+++ General/QA/Selenium.mm
@@ -0,0 +1,101 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#990099" CREATED="1361878162081" ID="ID_1703692073" LINK="../QA.mm" MODIFIED="1362156955584" TEXT="Selenium">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1362156875647" ID="ID_1190320724" MODIFIED="1362156948176" POSITION="left" TEXT="selectors">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1362065583400" FOLDED="true" ID="ID_1508722303" LINK="http://sauceio.com/index.php/2010/01/selenium-totw-css-selectors-in-selenium-demystified/" MODIFIED="1362072874998">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#009999">Sauce Labs</font></b>&#160;custom selectors
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1362072576969" MODIFIED="1362072639396">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      css=a[id^='id_prefix_']&#160;&#160;A link with an id that starts with the text id_prefix_.&#160;&#160;
+    </p>
+    <p>
+      css=a[id$='_id_sufix']&#160;&#160;A link with an id that ends with the text _id_sufix.&#160;&#160;
+    </p>
+    <p>
+      css=a[id*='id_pattern'] - A link with an id that ends with the id_pattern
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1362072681041" ID="ID_1405654411" LINK="http://sauceio.com/index.php/2010/03/selenium-tips-finding-elements-by-their-inner-text-using-contains-a-css-pseudo-class/" MODIFIED="1362072707612">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#009999">Sauce Labs</font></b>&#160;contains text
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1362156845442" ID="ID_1918777985" MODIFIED="1362156980241">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      #select anchor with img tag @StackOverflow
+    </p>
+    <p>
+      <font color="#0033cc">selenium.click(&quot;//a[@title='Click for the Manual Class LOV']/img&quot;);</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1362156199160" ID="ID_555750420" MODIFIED="1362156203787" POSITION="right" TEXT="selenium server">
+<node CREATED="1362156204090" MODIFIED="1362156221880">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # hub
+    </p>
+    <p>
+      <font color="#0033cc">http://127.0.0.1:4444/wd/hub/static/resource/hub.html</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1362072849759" ID="ID_1376212023" LINK="http://sauceio.com/" MODIFIED="1362072872102" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#009999">Sauce Lab</font></b>&#160;blog
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1362156882837" ID="ID_1321311435" MODIFIED="1362156895120" POSITION="right" TEXT="Firefox broser addons">
+<node CREATED="1361877855022" LINK="http://sebuilder.github.com/se-builder/" MODIFIED="1361877864508" TEXT="Selenium Builder"/>
+<node CREATED="1361878943818" MODIFIED="1361878948550" TEXT="Selenium IDE"/>
+</node>
+</node>
+</map>
