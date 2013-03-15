95f99dfd2f8d46b5d85ed62e9f6c8ae86f2e5743
Oleh Demyanyuk <olkobg@gmail.com>
2013-03-15 13:47:38 +0200

    -Addded mac keys for capturing screenshots. -Added watir and windmil UI QA links. -Added xpath innner ele selection and parent

--- OS/MacOS/MacOS.mm
+++ OS/MacOS/MacOS.mm
@@ -1,8 +1,9 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1316179329941" ID="ID_409853195" LINK="../../Index.mm" MODIFIED="1343378780192" TEXT="MacOS">
+<node CREATED="1316179329941" ID="ID_409853195" LINK="../../Index.mm" MODIFIED="1363347987174" TEXT="MacOS">
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
-<node CREATED="1316179374343" ID="ID_1570912865" MODIFIED="1360702210793" POSITION="right" TEXT="win keys">
+<node CREATED="1363347987157" FOLDED="true" ID="ID_82370541" MODIFIED="1363348049344" POSITION="right" TEXT="keys">
+<node CREATED="1316179374343" ID="ID_1570912865" MODIFIED="1360702210793" TEXT="win keys">
 <node CREATED="1316179383238" ID="ID_1611495993" MODIFIED="1316179388588" TEXT="Windows">
 <node CREATED="1316179388829" ID="ID_1034038969" MODIFIED="1316179398851" TEXT="Command"/>
 </node>
@@ -14,6 +15,31 @@
 </node>
 <node CREATED="1316179416802" ID="ID_846942676" MODIFIED="1316179419281" TEXT="Control">
 <node CREATED="1316179419577" ID="ID_1408461364" MODIFIED="1316179422056" TEXT="Control"/>
+</node>
+</node>
+<node CREATED="1363347998488" FOLDED="true" ID="ID_1044927767" MODIFIED="1363348047757" TEXT="screenshot">
+<node CREATED="1363347989921" ID="ID_768097240" MODIFIED="1363348046665">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # full screen
+    </p>
+    <p>
+      <font color="#0033ff">Command + Shift + 3</font>
+    </p>
+    <p>
+      # custom window
+    </p>
+    <p>
+      <font color="#0033ff">Command + Shift + 4</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 <node CREATED="1343289776094" FOLDED="true" ID="ID_1441127359" MODIFIED="1360702168274" POSITION="left" TEXT="NFS">
--- X_HTML/XML.mm
+++ X_HTML/XML.mm
@@ -14,7 +14,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1298903350178" ID="ID_344704111" MODIFIED="1362507017911" POSITION="left" TEXT="Xpath">
+<node CREATED="1298903350178" ID="ID_344704111" MODIFIED="1363365961527" POSITION="left" TEXT="Xpath">
 <node CREATED="1362506900818" ID="ID_770134559" MODIFIED="1362507016394">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -28,11 +28,52 @@
       <font color="#0033ff">someelemtn<b>/following-sibling::a[1]</b></font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node COLOR="#0033ff" CREATED="1362507889115" ID="ID_139764145" MODIFIED="1362507896073" TEXT="preciding-sibling">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1363365844001" ID="ID_1623465910" MODIFIED="1363365959472">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;!-- get child element --&gt;
+    </p>
+    <p>
+      <b><font color="#0033ff">//myparent/mychild[. = 'foo'] </font></b>
+    </p>
+    <p>
+      &lt;!-- get parent element with specific child --&gt;
+    </p>
+    <p>
+      <b><font color="#0033ff">//myparent/mychild[. = 'foo'] </font></b>
+    </p>
+    <p>
+      <b><font color="#0033ff">//myparent/mychild[text( ) = 'foo']</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1363366235032" ID="ID_473406210" MODIFIED="1363366312492">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;!-- firset element wit spec attrubute --&gt;
+    </p>
+    <p>
+      <font color="#0033ff">//bookstore/book<b>[@location='US'][1]</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
 </node>
 <node CREATED="1298903424927" ID="ID_1661081815" MODIFIED="1298903427193" POSITION="left" TEXT="Xform"/>
