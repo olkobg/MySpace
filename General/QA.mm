5243734421b34f29f7de9c4ce6a4d8f1c5116216
Oleh Demyanyuk <olkobg@gmail.com>
2013-04-22 17:53:25 +0300

    -Added sikuli sctipt site

--- General/QA.mm
+++ General/QA.mm
@@ -5,13 +5,26 @@
 <node CREATED="1361879742021" FOLDED="true" ID="ID_1568052229" MODIFIED="1365768563338" POSITION="right" TEXT="Learn">
 <node CREATED="1356437553324" ID="ID_1638441621" LINK="http://istqb.org/" MODIFIED="1356437577574" TEXT="ISTQB"/>
 </node>
-<node CREATED="1361878157495" FOLDED="true" ID="ID_1954151142" MODIFIED="1365788869752" POSITION="left" TEXT="UI Testing">
+<node CREATED="1361878157495" ID="ID_1954151142" MODIFIED="1366642305210" POSITION="left" TEXT="UI Testing">
 <node CREATED="1362156937205" ID="ID_327147454" LINK="QA/Selenium.mm" MODIFIED="1362156937206" TEXT="Selenium"/>
 <node CREATED="1363193756256" ID="ID_1318407635" LINK="http://www.autoitscript.com/site/autoit/" MODIFIED="1363193764353" TEXT="AutoitScript"/>
 <node CREATED="1363350370142" ID="ID_556506093" LINK="http://watir.com/" MODIFIED="1363350373264" TEXT="Watir"/>
 <node CREATED="1363350383839" ID="ID_92704623" LINK="http://www.getwindmill.com/" MODIFIED="1363350387598" TEXT="windMill"/>
+<node CREATED="1366642306310" ID="ID_684028615" LINK="http://www.sikuli.org/" MODIFIED="1366642375554">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#009999">Sikuli script</font>&#160;</b>uses <b><font color="#0033ff">python</font></b>&#160;and has <b><font color="#0033ff">java</font></b>&#160; api
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node CREATED="1364912707566" FOLDED="true" ID="ID_1232572713" MODIFIED="1365768561158" POSITION="left" TEXT="unit testing">
+</node>
+<node CREATED="1364912707566" FOLDED="true" ID="ID_1232572713" MODIFIED="1366642305035" POSITION="left" TEXT="unit testing">
 <node CREATED="1364912718331" FOLDED="true" ID="ID_663292656" MODIFIED="1365768560599" TEXT="xUnit">
 <node CREATED="1364912726578" ID="ID_536098144" MODIFIED="1365520997085" TEXT="JUnit"/>
 <node CREATED="1365520932207" FOLDED="true" ID="ID_1725699297" LINK="../PHP/PHPUnit.mm" MODIFIED="1365768560389" TEXT="PHPUnit">
