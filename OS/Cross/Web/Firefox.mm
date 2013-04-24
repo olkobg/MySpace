a7b5f4024f8df5f47d8cbfb205f06ab625781ebe
Oleh Demyanyuk <olkobg@gmail.com>
2013-04-24 19:34:44 +0300

    -Updated firebug change frame info.

--- OS/Cross/Web/Firefox.mm
+++ OS/Cross/Web/Firefox.mm
@@ -20,14 +20,26 @@
 <node CREATED="1364305457823" ID="ID_1939847957" MODIFIED="1364305506165" POSITION="right" TEXT="firebug">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1364305512650" ID="ID_1463867361" MODIFIED="1364305519167" TEXT="console">
-<node CREATED="1364305522163" ID="ID_180211030" LINK="http://www.softwareishard.com/blog/planet-mozilla/firebug-tip-using-command-line-within-an-iframe/" MODIFIED="1364305574643">
+<node CREATED="1364305522163" ID="ID_180211030" LINK="http://www.softwareishard.com/blog/planet-mozilla/firebug-tip-using-command-line-within-an-iframe/" MODIFIED="1366816316510">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <font color="#009999"><b>softwareishard</b></font>&#160;change frame and window
+      <font color="#000000"><b>//</b></font><b><font color="#009999">&#160;softwareishard</font></b>&#160;change frame and window
+    </p>
+    <p>
+      <font color="#0033ff"><b>cd</b>(document.getElementById(&quot;frameID&quot;).contentWindow);</font>&#160;// switch to a frame by ID
+    </p>
+    <p>
+      <font color="#0033ff"><b>cd</b>(window.frames[0]);&#160;&#160;&#160;&#160; </font>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160; &#160;&#160;&#160;&#160;// switch to the first frame in the list of frames
+    </p>
+    <p>
+      <font color="#0033cc"><b>cd</b>(window.frames[&quot;frameName&quot;]);&#160;</font>&#160;&#160;&#160;&#160;&#160;&#160;&#160;// switch to a frame using by name
+    </p>
+    <p>
+      <font color="#0033ff"><b>cd</b>(window.top);&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160; </font>&#160; //switching back to the top level window
     </p>
   </body>
 </html>
