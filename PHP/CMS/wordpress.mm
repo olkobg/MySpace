dc57491dfe4c7aebe74cdb9ea17bbe0a7666febb
Oleh Demyanyuk <olkobg@gmail.com>
2013-05-09 10:29:54 +0300

    -Added wordpress multi url info

--- PHP/CMS/wordpress.mm
+++ PHP/CMS/wordpress.mm
@@ -6,5 +6,19 @@
 <node CREATED="1355995391192" ID="ID_560583564" LINK="http://wordpresso.org/tutorials/kuda-vstavlyat-etot-kod-v-wordpress-obshhaya-struktura-faylov-temyi/" MODIFIED="1355995400106" TEXT="theme files"/>
 <node CREATED="1355995401069" ID="ID_37526855" LINK="http://wordpresso.org/tutorials/kuda-vstavlyat-etot-kod-v-wordpress-obshhaya-struktura-faylov-temyi/" MODIFIED="1355995411501" TEXT="social nets"/>
 </node>
+<node CREATED="1367419824095" ID="ID_1090806974" MODIFIED="1367419881673" POSITION="left" TEXT="config">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1367419828814" ID="ID_1829088810" MODIFIED="1367419832964" TEXT="change site url">
+<node COLOR="#009999" CREATED="1367419833911" ID="ID_50442241" LINK="v" MODIFIED="1367419878155" TEXT="warpconduit">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#009999" CREATED="1367419857304" ID="ID_744451692" LINK="http://www.inmotionhosting.com/support/website/wordpress/wordpress-changing-the-site-url-and-home-settings" MODIFIED="1367419878157" TEXT="inmotion hosting">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#009999" CREATED="1367419869212" ID="ID_1387616713" LINK="http://codex.wordpress.org/Changing_The_Site_URL" MODIFIED="1367419878157" TEXT="wp codex">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
 </node>
 </map>
