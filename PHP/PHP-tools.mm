07f22b5a3fef8be7df4b51bbe54e8395bd100815
Oleh Demyanyuk <olkobg@gmail.com>
2013-09-11 20:09:57 +0300

    -Updated pear commands style

--- PHP/PHP-tools.mm
+++ PHP/PHP-tools.mm
@@ -40,43 +40,67 @@
   </body>
 </html></richcontent>
 </node>
-<node COLOR="#0033ff" CREATED="1299137833498" FOLDED="true" ID="ID_1278823281" MODIFIED="1370531427110" POSITION="left" TEXT="PEAR">
+<node COLOR="#0033ff" CREATED="1299137833498" FOLDED="true" ID="ID_1278823281" MODIFIED="1378917945713" POSITION="left" TEXT="PEAR">
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
-<node COLOR="#0033ff" CREATED="1299137840048" ID="ID_1776483513" MODIFIED="1370531422977" TEXT="channel">
+<node COLOR="#0033ff" CREATED="1299137840048" FOLDED="true" ID="ID_1776483513" MODIFIED="1378917943958" TEXT="channel">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node COLOR="#0033ff" CREATED="1299137843737" ID="ID_1148392153" MODIFIED="1362073697551" TEXT="channel-discover">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node COLOR="#0033ff" CREATED="1299137871096" ID="ID_800257669" MODIFIED="1362073704350">
+<node COLOR="#0033ff" CREATED="1299137871096" ID="ID_800257669" MODIFIED="1378915450049">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b>pear channel-discover</b>&#160;pear.phpunit.de&#160;
+      $ <b>pear channel-discover</b>&#160;pear.phpunit.de&#160;
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1361449411358" ID="ID_1072674554" MODIFIED="1370531422977" TEXT="list">
+<node COLOR="#0033ff" CREATED="1361449411358" FOLDED="true" ID="ID_1072674554" MODIFIED="1378917943710" TEXT="remote-list">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1361449173409" ID="ID_749146692" MODIFIED="1362073732859">
+<node CREATED="1361449173409" ID="ID_749146692" MODIFIED="1378915444243">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <font color="#0033ff">pear <b>remote-list</b>&#160;<b>-c</b>&#160;phpunit</font>
+      <font color="#000000"># show available packages from channel</font>
+    </p>
+    <p>
+      <font color="#0033ff">$ pear <b>remote-list</b>&#160;<b>-c</b>&#160;phpunit</font>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1370531375079" ID="ID_1880740945" MODIFIED="1370531422978" TEXT="uninstall">
+<node COLOR="#0033ff" CREATED="1378915325684" FOLDED="true" ID="ID_1932880357" MODIFIED="1378917943485" TEXT="list">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1378915339207" ID="ID_534482124" MODIFIED="1378915408741">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000000"># show installed packages per channel</font>
+    </p>
+    <p>
+      <font color="#0033ff">$ pear <b>list -c</b>&#160;pecl</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1370531375079" FOLDED="true" ID="ID_1880740945" MODIFIED="1378917943238" TEXT="uninstall">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1370531379998" ID="ID_746509568" MODIFIED="1370531411890">
 <richcontent TYPE="NODE"><html>
@@ -88,26 +112,26 @@
       <font color="#0033ff">$ pear <b>unintstall</b>&#160;element-34/PHPWebDriver</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1362073629236" ID="ID_1946728433" MODIFIED="1362073755834" TEXT="packages">
+<node CREATED="1362073629236" FOLDED="true" ID="ID_1946728433" MODIFIED="1378917940898" TEXT="packages">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node COLOR="#0033ff" CREATED="1362073755824" ID="ID_1274603558" MODIFIED="1362073775082" TEXT="pear">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1362073632385" ID="ID_555496264" MODIFIED="1362073639582" TEXT="phpDocumentor">
-<node CREATED="1362073642292" ID="ID_1229348179" MODIFIED="1362073667606">
+<node CREATED="1362073642292" ID="ID_1229348179" MODIFIED="1378915456662">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b><font color="#0033ff">pear install </font></b><font color="#0033ff">phpDocumentor</font>
+      <font color="#0033ff">$ <b>pear install </b>phpDocumentor</font>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -118,7 +142,7 @@
 <node COLOR="#0033ff" CREATED="1362073760223" ID="ID_128298590" MODIFIED="1362073775081" TEXT="symfony">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1363200200899" ID="ID_973196156" MODIFIED="1363200203837" TEXT="config">
+<node CREATED="1363200200899" FOLDED="true" ID="ID_973196156" MODIFIED="1378917940088" TEXT="config">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1363200205478" ID="ID_199410938" LINK="http://pear.php.net/manual/en/guide.users.commandline.config.php" MODIFIED="1363200214994" TEXT="config options"/>
 </node>
