9838a1838075c1741dbbafe4e84843b135c5bdbb
Oleh Demyanyuk <olkobg@gmail.com>
2013-11-29 11:59:41 +0200

    -Converted windows maps to v1

--- OS/WinOs/windows 7.mm
+++ OS/WinOs/windows 7.mm
@@ -1,4 +1,4 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#006633" CREATED="1294074191258" ID="ID_1534815671" LINK="Wlike.mm" MODIFIED="1355337440815" TEXT="Win 7">
 <font BOLD="true" NAME="SansSerif" SIZE="17"/>
@@ -9,8 +9,8 @@
 <node CREATED="1355337393547" ID="ID_180966757" LINK="http://forums.mydigitallife.info/threads/24214-MBR-Regenerator-The-Professional-Windows-Fix-Tool" MODIFIED="1355337459252" TEXT="MBR Regenerator"/>
 <node CREATED="1355337409749" ID="ID_82988919" LINK="http://forums.mydigitallife.info/threads/26994-WAT-Fix-Fix-your-activation-problems" MODIFIED="1355337472444" TEXT="WAT Fix"/>
 </node>
-<node CREATED="1334745723015" FOLDED="true" ID="ID_1837994311" MODIFIED="1373353158688" POSITION="left" TEXT="Repair Tips">
-<node CREATED="1334745759104" FOLDED="true" ID="ID_800339970" LINK="http://www.pcworld.com/downloads/file/fid,171319/description.html" MODIFIED="1355432028325" TEXT="boot regitsry editor">
+<node CREATED="1334745723015" FOLDED="true" ID="ID_1837994311" MODIFIED="1385715776571" POSITION="left" TEXT="Repair Tips">
+<node CREATED="1334745759104" FOLDED="true" ID="ID_800339970" LINK="http://www.pcworld.com/downloads/file/fid,171319/description.html" MODIFIED="1385715680798" TEXT="boot regitsry editor">
 <node CREATED="1334745732988" ID="ID_938314211" MODIFIED="1355337378216">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -18,21 +18,21 @@
   </head>
   <body>
     <p>
-      Boot the CD and follow these instructions. I've put the on-screen prompts in&#160;italics.&#160;After you type your answer, press&#160;Enter.&#160;<br />
+      Boot the CD and follow these instructions. I've put the on-screen prompts in&#xa0;italics.&#xa0;After you type your answer, press&#xa0;Enter.&#xa0;<br/>
     </p>
     <p>
-      &#160;boot:&#160;Just press&#160;Enter.&#160;<br /><br />&#160;Select: [1]: Above the prompt you'll see a list of hard-drive partitions. Select the right one by typing that number.&#160;<br /><br />&#160;What is the path to the registry directory?...: The default is probably correct. Just press&#160;Enter.&#160;<br /><br />&#160;[1]:&#160;1&#160;<br /><br />&#160;What to do? [1] -&gt;:&#160;1&#160;<br /><br />&#160;or simply enter the username...: Type the name of the administrator account. If you're not sure what it is, all of the account names are listed above the prompt.&#160;<br /><br />&#160;Select: [q] &gt;:&#160;1&#160;<br /><br />&#160;Select: ! - quit...:&#160;!&#160;<br /><br />&#160;What to do [1]:&#160;q&#160;<br /><br />&#160;About to write file(s) back...:&#160;y&#160;<br /><br />&#160;New run? [n]:&#160;n&#160;<br /><br />&#160;#&#160;Remove the CD and reboot.
+      &#xa0;boot:&#xa0;Just press&#xa0;Enter.&#xa0;<br/><br/>&#xa0;Select: [1]: Above the prompt you'll see a list of hard-drive partitions. Select the right one by typing that number.&#xa0;<br/><br/>&#xa0;What is the path to the registry directory?...: The default is probably correct. Just press&#xa0;Enter.&#xa0;<br/><br/>&#xa0;[1]:&#xa0;1&#xa0;<br/><br/>&#xa0;What to do? [1] -&gt;:&#xa0;1&#xa0;<br/><br/>&#xa0;or simply enter the username...: Type the name of the administrator account. If you're not sure what it is, all of the account names are listed above the prompt.&#xa0;<br/><br/>&#xa0;Select: [q] &gt;:&#xa0;1&#xa0;<br/><br/>&#xa0;Select: ! - quit...:&#xa0;!&#xa0;<br/><br/>&#xa0;What to do [1]:&#xa0;q&#xa0;<br/><br/>&#xa0;About to write file(s) back...:&#xa0;y&#xa0;<br/><br/>&#xa0;New run? [n]:&#xa0;n&#xa0;<br/><br/>&#xa0;#&#xa0;Remove the CD and reboot.
     </p>
   </body>
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1334745974774" FOLDED="true" ID="ID_1330143835" MODIFIED="1364843807428" TEXT="restoring Files">
+<node CREATED="1334745974774" FOLDED="true" ID="ID_1330143835" MODIFIED="1385715682430" TEXT="restoring Files">
 <node CREATED="1334745988096" ID="ID_459900079" LINK="http://www.pcworld.com/downloads/file/fid,82979/description.html" MODIFIED="1334746016506" TEXT="Recuva Portable"/>
 </node>
-<node CREATED="1373353044981" FOLDED="true" ID="ID_200513431" MODIFIED="1373353157623" TEXT="paging file">
+<node CREATED="1373353044981" ID="ID_200513431" MODIFIED="1385715682633" TEXT="paging file">
 <node CREATED="1373353067018" ID="ID_1721969244" LINK="http://forum.thewindowsclub.com/windows-tips-tutorials-articles/32471-disable-delete-recreate-paging-file-windows-7-a.html" MODIFIED="1373353073104" TEXT="disable"/>
-<node CREATED="1373353074065" FOLDED="true" ID="ID_290369023" MODIFIED="1373353157222" TEXT="clear">
+<node CREATED="1373353074065" FOLDED="true" ID="ID_290369023" MODIFIED="1385715686461" TEXT="clear">
 <node CREATED="1373353120378" ID="ID_1178758554" MODIFIED="1373353150545">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -64,20 +64,34 @@
       Value: <b>1</b>
     </p>
   </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1385715693188" FOLDED="true" ID="ID_187361126" MODIFIED="1385715776154" TEXT="/etc/hosts/">
+<node CREATED="1385715700772" ID="ID_1936245790" MODIFIED="1385715770672">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      to edit run <b><font color="#0033ff">notepad</font></b>&#160;as <b>administrator</b>
+    </p>
+  </body>
 </html>
 </richcontent>
 </node>
 </node>
 </node>
-</node>
 <node CREATED="1355828454112" ID="ID_976125818" MODIFIED="1373353034900" POSITION="left" TEXT="drivers">
-<node CREATED="1355828457438" FOLDED="true" ID="ID_759658394" MODIFIED="1373353040664" TEXT="keyboard">
-<node CREATED="1355828460995" FOLDED="true" ID="ID_415635903" MODIFIED="1373353039675" TEXT="Apple wireless">
+<node CREATED="1355828457438" FOLDED="true" ID="ID_759658394" MODIFIED="1385715677142" TEXT="keyboard">
+<node CREATED="1355828460995" FOLDED="true" ID="ID_415635903" MODIFIED="1385715672597" TEXT="Apple wireless">
 <node CREATED="1355828468154" ID="ID_1249199752" LINK="http://code.google.com/p/uawks/" MODIFIED="1355828473592" TEXT="UAWKS"/>
 <node CREATED="1355828801616" ID="ID_1660368325" LINK="http://www.redmondpie.com/use-apple-wireless-keyboard-on-windows-with-full-functionality-using-apple-wireless-keyboard-helper/" MODIFIED="1355828811625" TEXT="Apple Wireless Keyboard Helper "/>
 </node>
-<node CREATED="1364843119245" FOLDED="true" ID="ID_402294443" MODIFIED="1373353037981" TEXT="tools">
-<node CREATED="1364843122008" FOLDED="true" ID="ID_1224146894" LINK="http://www.autohotkey.com/" MODIFIED="1373353037755" TEXT="autohotkey">
+<node CREATED="1364843119245" ID="ID_402294443" MODIFIED="1385715673137" TEXT="tools">
+<node CREATED="1364843122008" FOLDED="true" ID="ID_1224146894" LINK="http://www.autohotkey.com/" MODIFIED="1385715674733" TEXT="autohotkey">
 <node CREATED="1364843983812" ID="ID_1962272818" LINK="http://superuser.com/questions/486426/mapping-ctrlarrow-for-home-end-pageup-down" MODIFIED="1364844011924">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -85,14 +99,14 @@
   </head>
   <body>
     <p>
-      <b><font color="#0000ff">su</font></b>&#160;article change pgDn..
+      <b><font color="#0000ff">su</font></b>&#xa0;article change pgDn..
     </p>
   </body>
 </html></richcontent>
 </node>
 <node CREATED="1364844033569" ID="ID_1785649496" LINK="http://www.daonlyfreez.net/tutorials/3p/Veil/fnkey.htm" MODIFIED="1364844043570" TEXT="article about Fn key"/>
 </node>
-<node CREATED="1364843179426" FOLDED="true" ID="ID_1847816267" LINK="http://webpages.charter.net/krumsick/" MODIFIED="1364843803984" TEXT="keytweak">
+<node CREATED="1364843179426" FOLDED="true" ID="ID_1847816267" LINK="http://webpages.charter.net/krumsick/" MODIFIED="1385715676093" TEXT="keytweak">
 <node CREATED="1364843781044" ID="ID_238107989" MODIFIED="1364843802592">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -100,7 +114,7 @@
   </head>
   <body>
     <p>
-      <b><font color="#009999">lifehacker</font></b>&#160;keytweaker
+      <b><font color="#009999">lifehacker</font></b>&#xa0;keytweaker
     </p>
   </body>
 </html></richcontent>
