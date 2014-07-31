11bd2295b8aa0bad6997dec8607953c7877ef78a
Oleh Demyanyuk <olkobg@gmail.com>
2014-07-31 20:02:25 +0300

    -Added Ruby gem bundler info

--- Ruby&Rails/Gems.mm
+++ Ruby&Rails/Gems.mm
@@ -1,22 +1,26 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1357147320972" ID="ID_145689485" LINK="ruby.mm" MODIFIED="1378898924385" TEXT="Gems">
 <font BOLD="true" NAME="SansSerif" SIZE="15"/>
-<node CREATED="1378898924370" ID="ID_1330484426" MODIFIED="1378898930242" POSITION="right" TEXT="multiple install">
-<node CREATED="1357725946061" ID="ID_1419283319" LINK="http://gembundler.com/" MODIFIED="1357725993367">
+<node CREATED="1378898924370" FOLDED="true" ID="ID_1330484426" MODIFIED="1406798871100" POSITION="right" TEXT="manage">
+<node CREATED="1357725946061" FOLDED="true" ID="ID_1419283319" LINK="http://gembundler.com/" MODIFIED="1406798870895">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      gem <font color="#0033ff"><b>bundler</b></font>
+      <font color="#0033ff"><b>bundler</b></font>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
+<node COLOR="#0033ff" CREATED="1406798861870" ID="ID_131045885" MODIFIED="1406798867312" TEXT="$ bundle">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1378898934023" ID="ID_729324241" MODIFIED="1378898935598" POSITION="left" TEXT="libs">
+</node>
+<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1406798872968" POSITION="left" TEXT="libs">
 <node COLOR="#009999" CREATED="1378898936542" ID="ID_1270190876" LINK="http://www.thoughtbot.com/community" MODIFIED="1378898965974" TEXT="thoughtbot comunity">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
