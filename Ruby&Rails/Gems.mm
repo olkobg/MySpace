3d25f26e81b46ff8c936570d0b6acf62d2fca695
Oleh Demyanyuk <olkobg@gmail.com>
2014-10-15 12:08:51 +0300

    -Added command to update gem system

--- Ruby&Rails/Gems.mm
+++ Ruby&Rails/Gems.mm
@@ -1,9 +1,9 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1357147320972" ID="ID_145689485" LINK="ruby.mm" MODIFIED="1378898924385" TEXT="Gems">
+<node CREATED="1357147320972" ID="ID_145689485" LINK="ruby.mm" MODIFIED="1412239747251" TEXT="Gems">
 <font BOLD="true" NAME="SansSerif" SIZE="15"/>
-<node CREATED="1378898924370" FOLDED="true" ID="ID_1330484426" MODIFIED="1406798871100" POSITION="right" TEXT="manage">
-<node CREATED="1357725946061" FOLDED="true" ID="ID_1419283319" LINK="http://gembundler.com/" MODIFIED="1406798870895">
+<node CREATED="1378898924370" FOLDED="true" ID="ID_1330484426" MODIFIED="1412239744636" POSITION="right" TEXT="manage">
+<node CREATED="1357725946061" FOLDED="true" ID="ID_1419283319" LINK="http://gembundler.com/" MODIFIED="1412239744447">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -13,14 +13,31 @@
       <font color="#0033ff"><b>bundler</b></font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node COLOR="#0033ff" CREATED="1406798861870" ID="ID_131045885" MODIFIED="1406798867312" TEXT="$ bundle">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </node>
-<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1406798872968" POSITION="left" TEXT="libs">
+<node CREATED="1412239748602" FOLDED="true" ID="ID_1659455024" MODIFIED="1412256540811" POSITION="right" TEXT="manager">
+<node CREATED="1412239752249" ID="ID_908788601" MODIFIED="1412239889113">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # install specific version
+    </p>
+    <p>
+      <font color="#0000ff">$ gem update --system 1.8.25</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1412239746819" POSITION="left" TEXT="libs">
 <node COLOR="#009999" CREATED="1378898936542" ID="ID_1270190876" LINK="http://www.thoughtbot.com/community" MODIFIED="1378898965974" TEXT="thoughtbot comunity">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
