492e6ae3ee9ed5b97d5310cfa86b84250bb91360
Oleh Demyanyuk <olkobg@gmail.com>
2014-10-15 12:07:07 +0300

    -Added new command options for rvm

--- Ruby&Rails/rvm.mm
+++ Ruby&Rails/rvm.mm
@@ -1,32 +1,36 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#0033ff" CREATED="1297156626966" ID="ID_1592838391" LINK="ruby.mm" MODIFIED="1357132498132" TEXT="rvm">
+<node COLOR="#0033ff" CREATED="1297156626966" ID="ID_1592838391" LINK="ruby.mm" MODIFIED="1412241756312" TEXT="rvm">
 <font BOLD="true" NAME="SansSerif" SIZE="17"/>
-<node CREATED="1356802583376" LINK="http://www.tiplite.com/how-to-install-rvm-on-windows-using-cygwin/" MODIFIED="1356802776901" POSITION="right">
+<node CREATED="1412241756290" ID="ID_1798756369" MODIFIED="1412241758060" POSITION="right" TEXT="install">
+<node CREATED="1412241760201" ID="ID_1615089304" MODIFIED="1412241763108" TEXT="cygwin">
+<node CREATED="1356802583376" ID="ID_1733994002" LINK="http://www.tiplite.com/how-to-install-rvm-on-windows-using-cygwin/" MODIFIED="1412241765579">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b>tiplite com:</b>&#160;install on cygwin
+      <b>tiplite com:</b>&#xa0;install on cygwin
     </p>
   </body>
 </html></richcontent>
 </node>
-<node CREATED="1356802778177" ID="ID_1098325489" LINK="http://blog.developwithpassion.com/2012/03/30/installing-rvm-with-cygwin-on-windows/" MODIFIED="1356802818207" POSITION="right">
+<node CREATED="1356802778177" ID="ID_1098325489" LINK="http://blog.developwithpassion.com/2012/03/30/installing-rvm-with-cygwin-on-windows/" MODIFIED="1412241765574">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b>developWithPatssion:</b>&#160;install on cygwin
+      <b>developWithPatssion:</b>&#xa0;install on cygwin
     </p>
   </body>
 </html></richcontent>
 </node>
-<node COLOR="#0033ff" CREATED="1357132428975" ID="ID_302013714" MODIFIED="1357132436116" POSITION="left" TEXT="imlode">
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1357132428975" FOLDED="true" ID="ID_302013714" MODIFIED="1412241769018" POSITION="left" TEXT="imlode">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1357132442947" ID="ID_581385452" MODIFIED="1357132492136">
 <richcontent TYPE="NODE"><html>
@@ -41,9 +45,15 @@
       <font color="#0033ff">rvm <b>implode</b></font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
+<node CREATED="1412193767223" ID="ID_1467509112" LINK="http://stackoverflow.com/questions/3558656/how-can-i-remove-rvm-ruby-version-manager-from-my-system" MODIFIED="1412193786048" TEXT="stack overflow"/>
+</node>
+<node COLOR="#0033ff" CREATED="1412241770113" ID="ID_1505649414" MODIFIED="1412241784882" POSITION="left" TEXT="list">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#0033ff" CREATED="1412241772200" ID="ID_1240835462" MODIFIED="1412241784883" POSITION="left" TEXT="use">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </map>
