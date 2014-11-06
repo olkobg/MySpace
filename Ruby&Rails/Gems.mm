26b4880fc27b986e98244401e91efc69545cb4c0
Oleh Demyanyuk <olkobg@gmail.com>
2014-11-06 11:00:02 +0200

    -Added new gems info

--- Ruby&Rails/Gems.mm
+++ Ruby&Rails/Gems.mm
@@ -2,8 +2,8 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1357147320972" ID="ID_145689485" LINK="ruby.mm" MODIFIED="1412239747251" TEXT="Gems">
 <font BOLD="true" NAME="SansSerif" SIZE="15"/>
-<node CREATED="1378898924370" FOLDED="true" ID="ID_1330484426" MODIFIED="1412239744636" POSITION="right" TEXT="manage">
-<node CREATED="1357725946061" FOLDED="true" ID="ID_1419283319" LINK="http://gembundler.com/" MODIFIED="1412239744447">
+<node CREATED="1378898924370" FOLDED="true" ID="ID_1330484426" MODIFIED="1413986499057" POSITION="right" TEXT="manage">
+<node CREATED="1357725946061" FOLDED="true" ID="ID_1419283319" LINK="http://gembundler.com/" MODIFIED="1413986498427">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -19,7 +19,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1412239748602" FOLDED="true" ID="ID_1659455024" MODIFIED="1412256540811" POSITION="right" TEXT="manager">
+<node CREATED="1412239748602" FOLDED="true" ID="ID_1659455024" MODIFIED="1413986499574" POSITION="right" TEXT="manager">
 <node CREATED="1412239752249" ID="ID_908788601" MODIFIED="1412239889113">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -33,13 +33,21 @@
       <font color="#0000ff">$ gem update --system 1.8.25</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1412239746819" POSITION="left" TEXT="libs">
+<node CREATED="1378898934023" ID="ID_729324241" MODIFIED="1413986500356" POSITION="left" TEXT="libs">
 <node COLOR="#009999" CREATED="1378898936542" ID="ID_1270190876" LINK="http://www.thoughtbot.com/community" MODIFIED="1378898965974" TEXT="thoughtbot comunity">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1413986503168" ID="ID_713345953" MODIFIED="1413986506901" POSITION="left" TEXT="dev">
+<node CREATED="1413986585226" ID="ID_1282665512" MODIFIED="1413986589726" TEXT="nice format">
+<node CREATED="1413986510031" ID="ID_627079957" LINK="https://github.com/cldwalker/hirb" MODIFIED="1413986616195" TEXT="Hirb">
+<node CREATED="1413986520538" ID="ID_650307305" MODIFIED="1413986531539" TEXT="better overview of models in rails"/>
+</node>
+<node CREATED="1413986597580" ID="ID_1909254068" LINK="http://stackoverflow.com/questions/1224525/how-to-get-nice-formatting-in-the-rails-console" MODIFIED="1413986603234" TEXT="yaml format"/>
+<node CREATED="1413986623748" ID="ID_827372704" LINK="https://github.com/michaeldv/awesome_print" MODIFIED="1413986635230" TEXT="awesome_print"/>
 </node>
 </node>
 </node>
