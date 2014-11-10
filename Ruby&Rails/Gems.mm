a9856c8f8fc751f52a8ef250fc571a6c533cf4da
Oleh Demyanyuk <olkobg@gmail.com>
2014-11-10 13:24:47 +0200

    -Added gem grape for making RestAPI

--- Ruby&Rails/Gems.mm
+++ Ruby&Rails/Gems.mm
@@ -36,18 +36,21 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1378898934023" ID="ID_729324241" MODIFIED="1413986500356" POSITION="left" TEXT="libs">
+<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1415618636988" POSITION="left" TEXT="libs">
 <node COLOR="#009999" CREATED="1378898936542" ID="ID_1270190876" LINK="http://www.thoughtbot.com/community" MODIFIED="1378898965974" TEXT="thoughtbot comunity">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1413986503168" ID="ID_713345953" MODIFIED="1413986506901" POSITION="left" TEXT="dev">
-<node CREATED="1413986585226" ID="ID_1282665512" MODIFIED="1413986589726" TEXT="nice format">
+<node CREATED="1413986503168" FOLDED="true" ID="ID_713345953" MODIFIED="1415618636284" POSITION="left" TEXT="dev">
+<node CREATED="1413986585226" FOLDED="true" ID="ID_1282665512" MODIFIED="1415618635995" TEXT="nice format">
 <node CREATED="1413986510031" ID="ID_627079957" LINK="https://github.com/cldwalker/hirb" MODIFIED="1413986616195" TEXT="Hirb">
 <node CREATED="1413986520538" ID="ID_650307305" MODIFIED="1413986531539" TEXT="better overview of models in rails"/>
 </node>
 <node CREATED="1413986597580" ID="ID_1909254068" LINK="http://stackoverflow.com/questions/1224525/how-to-get-nice-formatting-in-the-rails-console" MODIFIED="1413986603234" TEXT="yaml format"/>
 <node CREATED="1413986623748" ID="ID_827372704" LINK="https://github.com/michaeldv/awesome_print" MODIFIED="1413986635230" TEXT="awesome_print"/>
+</node>
+<node CREATED="1415618608856" FOLDED="true" ID="ID_324683494" MODIFIED="1415618635332" TEXT="REST API">
+<node CREATED="1415618620684" ID="ID_1250867020" LINK="https://github.com/intridea/grape" MODIFIED="1415618633906" TEXT="grape"/>
 </node>
 </node>
 </node>
