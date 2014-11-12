b821d474ef7fcca8513811bba3e0f6a76bcd12c5
Oleh Demyanyuk <olkobg@gmail.com>
2014-11-12 20:10:39 +0200

    -Added rake tool map

---
+++ Ruby&Rails/rake.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1412241675627" LINK="../../../../MySpace/IT/Ruby&amp;Rails/ruby.mm" MODIFIED="1415812005937" TEXT="rake"/>
+</map>
