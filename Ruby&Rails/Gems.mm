f17b53c44e6b341ff5f0e707979f60524c713b73
Oleh Demyanyuk <olkobg@gmail.com>
2014-11-13 12:43:53 +0200

    -Added grape article

--- Ruby&Rails/Gems.mm
+++ Ruby&Rails/Gems.mm
@@ -36,12 +36,12 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1415618636988" POSITION="left" TEXT="libs">
+<node CREATED="1378898934023" FOLDED="true" ID="ID_729324241" MODIFIED="1415875176083" POSITION="left" TEXT="libs">
 <node COLOR="#009999" CREATED="1378898936542" ID="ID_1270190876" LINK="http://www.thoughtbot.com/community" MODIFIED="1378898965974" TEXT="thoughtbot comunity">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1413986503168" FOLDED="true" ID="ID_713345953" MODIFIED="1415618636284" POSITION="left" TEXT="dev">
+<node CREATED="1413986503168" FOLDED="true" ID="ID_713345953" MODIFIED="1415875178000" POSITION="left" TEXT="dev">
 <node CREATED="1413986585226" FOLDED="true" ID="ID_1282665512" MODIFIED="1415618635995" TEXT="nice format">
 <node CREATED="1413986510031" ID="ID_627079957" LINK="https://github.com/cldwalker/hirb" MODIFIED="1413986616195" TEXT="Hirb">
 <node CREATED="1413986520538" ID="ID_650307305" MODIFIED="1413986531539" TEXT="better overview of models in rails"/>
@@ -49,8 +49,10 @@
 <node CREATED="1413986597580" ID="ID_1909254068" LINK="http://stackoverflow.com/questions/1224525/how-to-get-nice-formatting-in-the-rails-console" MODIFIED="1413986603234" TEXT="yaml format"/>
 <node CREATED="1413986623748" ID="ID_827372704" LINK="https://github.com/michaeldv/awesome_print" MODIFIED="1413986635230" TEXT="awesome_print"/>
 </node>
-<node CREATED="1415618608856" FOLDED="true" ID="ID_324683494" MODIFIED="1415618635332" TEXT="REST API">
-<node CREATED="1415618620684" ID="ID_1250867020" LINK="https://github.com/intridea/grape" MODIFIED="1415618633906" TEXT="grape"/>
+<node CREATED="1415618608856" FOLDED="true" ID="ID_324683494" MODIFIED="1415875173995" TEXT="REST API">
+<node CREATED="1415618620684" FOLDED="true" ID="ID_1250867020" LINK="https://github.com/intridea/grape" MODIFIED="1415875173723" TEXT="grape">
+<node COLOR="#009999" CREATED="1415875145347" ID="ID_833859942" LINK="http://armoredcode.com/blog/build-an-api-for-fun-with-grape/" MODIFIED="1415875169159" TEXT="armoredcode"/>
+</node>
 </node>
 </node>
 </node>
