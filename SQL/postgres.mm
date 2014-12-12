28792d2336fc0f0ea5e2a7f9bcc8263c33a96b61
Oleh Demyanyuk <olkobg@gmail.com>
2014-12-12 11:57:44 +0200

    -Added postgres map

---
+++ SQL/postgres.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1291299315068" ID="ID_883053579" LINK="../Index.mm" MODIFIED="1418041237252" TEXT="Postgres">
+<node CREATED="1418042221151" ID="ID_92498126" MODIFIED="1418042222246" POSITION="right" TEXT="install">
+<node CREATED="1418042222580" ID="ID_663046568" MODIFIED="1418042228493" TEXT="mac os x">
+<node COLOR="#009999" CREATED="1418042229222" ID="ID_1995410257" LINK="https://www.codefellows.org/blog/three-battle-tested-ways-to-install-postgresql" MODIFIED="1418042291871" TEXT="codeflows">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1418042882345" ID="ID_1084752170" MODIFIED="1418042888754" POSITION="left" TEXT="psql">
+<node CREATED="1418042889196" ID="ID_1495855500" MODIFIED="1418047175112" TEXT="\du  list users">
+<icon BUILTIN="folder"/>
+</node>
+<node CREATED="1418120918624" ID="ID_1675762974" MODIFIED="1418120926378" TEXT="help"/>
+</node>
+</node>
+</map>
