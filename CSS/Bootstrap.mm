47a87fb06409bb8cade521a47d8c46a057eb5a1f
Oleh Demyanyuk <olkobg@gmail.com>
2014-12-21 19:09:55 +0200

    -Added Bootstrap map

---
+++ CSS/Bootstrap.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1418634456386" ID="ID_339977225" LINK="../Index.mm" MODIFIED="1418634477472" TEXT="Bootstrap">
+<node CREATED="1418634479913" ID="ID_46895628" MODIFIED="1418634482232" POSITION="right" TEXT="libs">
+<node CREATED="1418634482679" ID="ID_1615082174" LINK="http://www.zamanak.ir/themes/zamanak/bootstrap-switch-3.0/" MODIFIED="1418634490167" TEXT="bootstrap-switch"/>
+</node>
+</node>
+</map>
