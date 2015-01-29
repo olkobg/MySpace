8fcbc1f5aba8221daf1bba73f1b703f93c296348
Oleh Demyanyuk <olkobg@gmail.com>
2015-01-29 17:07:36 +0200

    -Added links for shoulda gem

--- Ruby&Rails/gems/shoulda.mm
+++ Ruby&Rails/gems/shoulda.mm
@@ -1,7 +1,10 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#990000" CREATED="1420927484051" ID="ID_1337917364" LINK="RSpec.mm" MODIFIED="1422537350486" TEXT="shoulda">
+<node COLOR="#990000" CREATED="1420927484051" ID="ID_1337917364" LINK="RSpec.mm" MODIFIED="1422537847696" TEXT="shoulda">
 <font NAME="SansSerif" SIZE="24"/>
-<node COLOR="#0033ff" CREATED="1422537313581" ID="ID_910892107" LINK="https://github.com/thoughtbot/shoulda" MODIFIED="1422537329476" POSITION="right" TEXT="github"/>
+<node CREATED="1422537847682" FOLDED="true" ID="ID_1677664918" MODIFIED="1422537859172" POSITION="right" TEXT="github">
+<node COLOR="#0033ff" CREATED="1422537313581" ID="ID_910892107" LINK="https://github.com/thoughtbot/shoulda" MODIFIED="1422537845707" TEXT="shoulda"/>
+<node COLOR="#0033ff" CREATED="1422537817878" ID="ID_1253598537" LINK="https://github.com/thoughtbot/shoulda-matchers" MODIFIED="1422537842289" TEXT="shoulda-matchers"/>
+</node>
 </node>
 </map>
