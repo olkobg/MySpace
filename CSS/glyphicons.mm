9d4c083eb5c2c1f17640578ff78982637d206af5
Oleh Demyanyuk <olkobg@gmail.com>
2015-01-29 14:54:27 +0200

    -Added glyph icons

---
+++ CSS/glyphicons.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#006633" CREATED="1422534889809" ID="ID_1438524019" LINK="../Index.mm" MODIFIED="1422535435171" TEXT="glyphicons">
+<font NAME="SansSerif" SIZE="24"/>
+<node COLOR="#0033ff" CREATED="1422535437109" ID="ID_1653861341" LINK="http://marcoceppi.github.io/bootstrap-glyphicons/" MODIFIED="1422535469979" POSITION="right" TEXT="booststarp 3 glyphicons"/>
+<node COLOR="#0033ff" CREATED="1422535460203" ID="ID_867356128" LINK="http://glyphicons.com/" MODIFIED="1422535468889" POSITION="right" TEXT="glyphicons"/>
+<node COLOR="#0033ff" CREATED="1422535531730" ID="ID_282478942" LINK="http://fontawesome.io/" MODIFIED="1422535538800" POSITION="right" TEXT="font awesome"/>
+<node CREATED="1422535551290" FOLDED="true" ID="ID_362012941" MODIFIED="1422535562744" POSITION="left" TEXT="info">
+<node CREATED="1422535552449" ID="ID_93622727" LINK="http://fontawesome.bootstrapcheatsheets.com/" MODIFIED="1422535560053" TEXT="print cheatsheap"/>
+</node>
+</node>
+</map>
