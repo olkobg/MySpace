750f10678ca03813f01c080905559ad9a9cd1557
Oleh Demyanyuk <olkobg@gmail.com>
2015-01-29 17:09:40 +0200

    -Added bootstraps print cheat sheet links

--- CSS/Bootstrap.mm
+++ CSS/Bootstrap.mm
@@ -2,16 +2,17 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#006633" CREATED="1418634456386" ID="ID_339977225" LINK="../Index.mm" MODIFIED="1422535492291" TEXT="Bootstrap">
 <font NAME="SansSerif" SIZE="30"/>
-<node CREATED="1418634479913" FOLDED="true" ID="ID_46895628" MODIFIED="1422535718866" POSITION="right" TEXT="libs">
+<node CREATED="1418634479913" FOLDED="true" ID="ID_46895628" MODIFIED="1422536178755" POSITION="right" TEXT="libs">
 <node CREATED="1418634482679" ID="ID_1615082174" LINK="http://www.zamanak.ir/themes/zamanak/bootstrap-switch-3.0/" MODIFIED="1418634490167" TEXT="bootstrap-switch"/>
 </node>
-<node CREATED="1422535487161" FOLDED="true" ID="ID_1520260928" MODIFIED="1422535857073" POSITION="left" TEXT="info">
-<node CREATED="1422535501632" FOLDED="true" ID="ID_1409001872" MODIFIED="1422535856793" TEXT="cheat sheats">
-<node COLOR="#0033ff" CREATED="1422535508982" ID="ID_708061473" LINK="http://www.taesup.com/bootstrap/" MODIFIED="1422535517176" TEXT="taesup.com &gt; Bootstrap">
+<node CREATED="1422535487161" FOLDED="true" ID="ID_1520260928" MODIFIED="1422536146196" POSITION="left" TEXT="info">
+<node CREATED="1422535501632" FOLDED="true" ID="ID_1409001872" MODIFIED="1422536144921" TEXT="cheat sheats">
+<node COLOR="#0033ff" CREATED="1422535575190" ID="ID_506920421" LINK="http://www.cheatography.com/masonjo/cheat-sheets/bootstrap/" MODIFIED="1422535620014" TEXT="cheatography.com &gt; Masonjo &gt;Bootstrap"/>
+<node COLOR="#0033ff" CREATED="1422536120205" ID="ID_1385688599" LINK="http://creativealive.com/master-cheat-sheet-twitter-bootstrap-3-pdf-download/" MODIFIED="1422536138482" TEXT="creativealive.com &gt; Master-twitter-bootstrap-3"/>
+<node COLOR="#0033ff" CREATED="1422535697098" ID="ID_775951217" LINK="http://www.designfloat.com/blog/2013/12/05/bootstrap-cheatsheets/" MODIFIED="1422535709314" TEXT="designfloat.com &gt; Blog &gt; Bootstrap-cheatsheets"/>
+<node COLOR="#0033ff" CREATED="1422535508982" FOLDED="true" ID="ID_708061473" LINK="http://www.taesup.com/bootstrap/" MODIFIED="1422536143295" TEXT="taesup.com &gt; Bootstrap">
 <node COLOR="#0033ff" CREATED="1422535643423" ID="ID_1717951810" LINK="https://github.com/taesup/bootstrap-cheat-sheet" MODIFIED="1422535647319" TEXT="github"/>
 </node>
-<node COLOR="#0033ff" CREATED="1422535575190" ID="ID_506920421" LINK="http://www.cheatography.com/masonjo/cheat-sheets/bootstrap/" MODIFIED="1422535620014" TEXT="cheatography.com &gt; Masonjo &gt;Bootstrap"/>
-<node COLOR="#0033ff" CREATED="1422535697098" ID="ID_775951217" LINK="http://www.designfloat.com/blog/2013/12/05/bootstrap-cheatsheets/" MODIFIED="1422535709314" TEXT="designfloat.com &gt; Blog &gt; Bootstrap-cheatsheets"/>
 </node>
 <node CREATED="1422535744000" FOLDED="true" ID="ID_289813187" MODIFIED="1422535856226" TEXT="foundation compare">
 <node COLOR="#0033ff" CREATED="1422535754889" ID="ID_1778983342" LINK="https://scotch.io/tutorials/cheat-sheet-for-comparing-bootstrap-and-foundation-css-classes" MODIFIED="1422535776830" TEXT="scotch.io"/>
