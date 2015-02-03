783de7c3077169e19cf733f1a76b25d60314d254
Oleh Demyanyuk <olkobg@gmail.com>
2015-02-03 02:22:07 +0200

    -Add Bootstrap container info

--- CSS/Bootstrap.mm
+++ CSS/Bootstrap.mm
@@ -2,11 +2,22 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#006633" CREATED="1418634456386" ID="ID_339977225" LINK="../Index.mm" MODIFIED="1422535492291" TEXT="Bootstrap">
 <font NAME="SansSerif" SIZE="30"/>
-<node CREATED="1418634479913" FOLDED="true" ID="ID_46895628" MODIFIED="1422536178755" POSITION="right" TEXT="libs">
+<node CREATED="1418634479913" FOLDED="true" ID="ID_46895628" MODIFIED="1422914579899" POSITION="right" TEXT="libs">
 <node CREATED="1418634482679" ID="ID_1615082174" LINK="http://www.zamanak.ir/themes/zamanak/bootstrap-switch-3.0/" MODIFIED="1418634490167" TEXT="bootstrap-switch"/>
 </node>
-<node CREATED="1422535487161" FOLDED="true" ID="ID_1520260928" MODIFIED="1422536146196" POSITION="left" TEXT="info">
-<node CREATED="1422535501632" FOLDED="true" ID="ID_1409001872" MODIFIED="1422536144921" TEXT="cheat sheats">
+<node CREATED="1422914581330" ID="ID_796164166" MODIFIED="1422914586059" POSITION="left" TEXT="grid"/>
+<node CREATED="1422918536649" ID="ID_1816541465" MODIFIED="1422918546643" POSITION="left" TEXT="containers">
+<node COLOR="#0033ff" CREATED="1422918547484" FOLDED="true" ID="ID_1109755746" MODIFIED="1422918949712" TEXT=".container">
+<node CREATED="1422918927021" ID="ID_1193107103" MODIFIED="1422918933527" TEXT="centered on page"/>
+</node>
+<node COLOR="#0033ff" CREATED="1422918934374" FOLDED="true" ID="ID_1733596121" MODIFIED="1422918949396" TEXT=".container-fluid">
+<node CREATED="1422918942264" ID="ID_1852004168" MODIFIED="1422918948351" TEXT="take entire page"/>
+</node>
+</node>
+<node CREATED="1422914586409" ID="ID_1887155494" MODIFIED="1422914589547" POSITION="left" TEXT="elements"/>
+<node CREATED="1422914590147" ID="ID_963497050" MODIFIED="1422914594187" POSITION="left" TEXT="other stuff"/>
+<node CREATED="1422535487161" FOLDED="true" ID="ID_1520260928" MODIFIED="1422914580740" POSITION="right" TEXT="info">
+<node CREATED="1422535501632" FOLDED="true" ID="ID_1409001872" MODIFIED="1422914576830" TEXT="cheat sheats">
 <node COLOR="#0033ff" CREATED="1422535575190" ID="ID_506920421" LINK="http://www.cheatography.com/masonjo/cheat-sheets/bootstrap/" MODIFIED="1422535620014" TEXT="cheatography.com &gt; Masonjo &gt;Bootstrap"/>
 <node COLOR="#0033ff" CREATED="1422536120205" ID="ID_1385688599" LINK="http://creativealive.com/master-cheat-sheet-twitter-bootstrap-3-pdf-download/" MODIFIED="1422536138482" TEXT="creativealive.com &gt; Master-twitter-bootstrap-3"/>
 <node COLOR="#0033ff" CREATED="1422535697098" ID="ID_775951217" LINK="http://www.designfloat.com/blog/2013/12/05/bootstrap-cheatsheets/" MODIFIED="1422535709314" TEXT="designfloat.com &gt; Blog &gt; Bootstrap-cheatsheets"/>
@@ -14,7 +25,7 @@
 <node COLOR="#0033ff" CREATED="1422535643423" ID="ID_1717951810" LINK="https://github.com/taesup/bootstrap-cheat-sheet" MODIFIED="1422535647319" TEXT="github"/>
 </node>
 </node>
-<node CREATED="1422535744000" FOLDED="true" ID="ID_289813187" MODIFIED="1422535856226" TEXT="foundation compare">
+<node CREATED="1422535744000" FOLDED="true" ID="ID_289813187" MODIFIED="1422914578029" TEXT="foundation compare">
 <node COLOR="#0033ff" CREATED="1422535754889" ID="ID_1778983342" LINK="https://scotch.io/tutorials/cheat-sheet-for-comparing-bootstrap-and-foundation-css-classes" MODIFIED="1422535776830" TEXT="scotch.io"/>
 </node>
 </node>
