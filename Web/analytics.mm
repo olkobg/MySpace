66e556c5798d6de309edb44023ee511dc9bc1e70
Oleh Demyanyuk <olkobg@gmail.com>
2015-02-10 16:52:38 +0200

    -Added web analytics map

---
+++ Web/analytics.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#338800" CREATED="1423577810894" ID="ID_1918480243" LINK="../Index.mm" MODIFIED="1423579883025" TEXT="analytics">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1423579885137" FOLDED="true" ID="ID_29563265" MODIFIED="1423579936792" POSITION="right" TEXT="info">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1423579904028" FOLDED="true" ID="ID_1365099493" MODIFIED="1423579936457" TEXT="utm params">
+<node COLOR="#0033ff" CREATED="1423579920152" ID="ID_27085523" LINK="http://konacompany.com/importance-using-utm-parameters-track-marketing-campaigns-google-analytics/" MODIFIED="1423579924279" TEXT="konacompany.com &gt; Importance-using-utm-parameters-track-marketing-campaigns-google-analytics"/>
+<node COLOR="#0033ff" CREATED="1423579932020" ID="ID_884465587" LINK="http://konacompany.com/web-analytics-track-marketing-campaigns-using-utm-parameters-google-analytics/" MODIFIED="1423579934072" TEXT="konacompany.com &gt; Web-analytics-track-marketing-campaigns-using-utm-parameters-google-analytics"/>
+</node>
+</node>
+</node>
+</map>
