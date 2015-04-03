dcded8872fd15fe24e71fe0b7e928448d06f098a
Oleh Demyanyuk <olkobg@gmail.com>
2015-04-03 22:22:12 +0300

    -Added Sqlite map

---
+++ SQL/Sqlite.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1291299339368" ID="ID_665189885" LINK="../Index.mm" MODIFIED="1427814375157" TEXT="Sqlite">
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1427814327506" FOLDED="true" ID="ID_1950326568" MODIFIED="1427814385882" POSITION="right" TEXT="config">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#0033ff" CREATED="1427814329107" ID="ID_1358229887" MODIFIED="1427814349348" TEXT="$ ~/.sqliterc"/>
+<node COLOR="#0033ff" CREATED="1427814354396" ID="ID_1511499939" MODIFIED="1427814373715" TEXT=".headers on"/>
+<node COLOR="#0033ff" CREATED="1427814360587" ID="ID_1792144245" MODIFIED="1427814373716" TEXT=".mode column"/>
+</node>
+</node>
+</map>
