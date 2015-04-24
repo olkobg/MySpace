5c00a299dced5e68181e14507561cd51bb8c9d6f
Oleh Demyanyuk <olkobg@gmail.com>
2015-04-24 23:21:02 +0300

    Changed structure of sublime and vim maps

--- OS/Cross/Sublime.mm
+++ OS/Cross/Sublime.mm
@@ -1,11 +1,11 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#006633" CREATED="1348054359883" ID="ID_1290849412" LINK="../Crossplatform.mm" MODIFIED="1420822101181" TEXT="Sublime">
+<node COLOR="#006633" CREATED="1348054359883" ID="ID_1290849412" LINK="../Crossplatform.mm" MODIFIED="1429115188572" TEXT="Sublime">
 <font NAME="SansSerif" SIZE="23"/>
-<node CREATED="1348054383724" FOLDED="true" ID="ID_1207364740" MODIFIED="1420818729972" POSITION="right" TEXT="plugins">
+<node CREATED="1348054383724" FOLDED="true" ID="ID_1207364740" MODIFIED="1429115188307" POSITION="right" TEXT="plugins">
 <font NAME="SansSerif" SIZE="13"/>
-<node CREATED="1348054390991" FOLDED="true" ID="ID_789731113" MODIFIED="1418384519374" TEXT="work remote">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1348054390991" FOLDED="true" ID="ID_789731113" MODIFIED="1429115184203" TEXT="work remote">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1348054420597" ID="ID_432849325" LINK="http://urbangiraffe.com/2011/08/13/remote-editing-with-sublime-text-2/" MODIFIED="1348054479275" TEXT="simple ssh + python"/>
 <node CREATED="1348054452693" ID="ID_869312784" LINK="https://github.com/bcharbonnier/SublimeFileSync" MODIFIED="1378980749399">
 <richcontent TYPE="NODE"><html>
@@ -56,8 +56,8 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1378984838793" FOLDED="true" ID="ID_1333268103" MODIFIED="1381704085134" TEXT="code">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1378984838793" FOLDED="true" ID="ID_1333268103" MODIFIED="1429115184203" TEXT="code">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1363104282068" ID="ID_1515952988" MODIFIED="1381703755192" TEXT="alignment">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1363104288995" ID="ID_1422909372" LINK="http://wbond.net/sublime_packages/alignment" MODIFIED="1381703757268">
@@ -79,8 +79,8 @@
 <node CREATED="1368618847778" ID="ID_1399406178" LINK="https://github.com/SublimeText/Tag" MODIFIED="1368618870351" TEXT="Tag"/>
 </node>
 <node CREATED="1352715895899" ID="ID_1829880842" LINK="http://wbond.net/sublime_packages/package_control" MODIFIED="1378984849766" TEXT="Plugin Controll"/>
-<node CREATED="1356448658853" FOLDED="true" ID="ID_179771754" MODIFIED="1381704098943" TEXT="validation">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1356448658853" FOLDED="true" ID="ID_179771754" MODIFIED="1429115184203" TEXT="validation">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1356448378788" ID="ID_1600803494" LINK="https://github.com/ehamiter/Sublime-Text-2-Plugins" MODIFIED="1356448393090" TEXT="Sublime-Text2-Plugins"/>
 <node CREATED="1356448393833" ID="ID_838555007" LINK="http://www.sublimetext.com/forum/viewtopic.php?f=5&amp;t=2262" MODIFIED="1356448418271" TEXT="Html/Css"/>
 <node CREATED="1378980388109" ID="ID_735756139" LINK="https://github.com/SublimeLinter/SublimeLinter" MODIFIED="1378980408476">
@@ -96,12 +96,12 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1357740650424" FOLDED="true" ID="ID_308180726" MODIFIED="1381704101480" TEXT="search">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1357740650424" FOLDED="true" ID="ID_308180726" MODIFIED="1429115184203" TEXT="search">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1357740654866" ID="ID_1981419594" LINK="http://www.sublimetext.com/forum/viewtopic.php?f=6&amp;t=9172" MODIFIED="1357740660143" TEXT="in all opened files"/>
 </node>
-<node CREATED="1358849186461" FOLDED="true" ID="ID_1642636777" MODIFIED="1381704074744" TEXT="reindent">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1358849186461" FOLDED="true" ID="ID_1642636777" MODIFIED="1429115184203" TEXT="reindent">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1358849190732" ID="ID_60933777" LINK="http://stackoverflow.com/questions/8839753/formatting-html-code-using-sublime-text-2" MODIFIED="1358849212343">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -115,8 +115,8 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1360703522221" FOLDED="true" ID="ID_627433256" MODIFIED="1381704073751" TEXT="sess">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1360703522221" FOLDED="true" ID="ID_627433256" MODIFIED="1429115184203" TEXT="sess">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1360703477511" ID="ID_1180477952" LINK="https://github.com/titoBouzout/BufferScrol" MODIFIED="1360703518535">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -130,8 +130,8 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1363106171499" FOLDED="true" ID="ID_43054520" MODIFIED="1381704072626" TEXT="build">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1363106171499" FOLDED="true" ID="ID_43054520" MODIFIED="1429115184203" TEXT="build">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1363106174420" ID="ID_541301195" LINK="https://github.com/stuartherbert/sublime-phpunit" MODIFIED="1363106233773">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -158,61 +158,7 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1353338690297" FOLDED="true" ID="ID_1889510088" LINK="http://www.sublimetext.com/docs/2/projects.html" MODIFIED="1420818742746" POSITION="left" TEXT="projects">
-<font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1378980254553" ID="ID_1209766017" MODIFIED="1378980316184">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      // simple project config
-    </p>
-    <p>
-      <font color="#0033ff">{ </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;"folders": </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;[ </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;{ </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;"follow_symlinks": true, </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;"path": "api-tests" </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;} </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;], </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;"settings": </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;{ </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;"tab_size": 2, </font>
-    </p>
-    <p>
-      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;} </font>
-    </p>
-    <p>
-      <font color="#0033ff">} </font>
-    </p>
-  </body>
-</html></richcontent>
-</node>
-</node>
-<node CREATED="1353338701198" FOLDED="true" ID="ID_182490694" LINK="http://net.tutsplus.com/tutorials/tools-and-tips/sublime-text-2-project-bliss/" MODIFIED="1420818757161" POSITION="left" TEXT="config">
+<node CREATED="1353338701198" FOLDED="true" ID="ID_182490694" LINK="http://net.tutsplus.com/tutorials/tools-and-tips/sublime-text-2-project-bliss/" MODIFIED="1429115339646" POSITION="left" TEXT="config">
 <font NAME="SansSerif" SIZE="12"/>
 <node COLOR="#0033ff" CREATED="1355140000578" ID="ID_624009682" MODIFIED="1381704791242">
 <richcontent TYPE="NODE"><html>
@@ -256,7 +202,7 @@
 <node COLOR="#0033ff" CREATED="1355140175527" ID="ID_1637713145" LINK="http://www.sublimetext.com/forum/viewtopic.php?f=3&amp;t=2691" MODIFIED="1420818746929" TEXT="highlight_line">
 <font NAME="SansSerif" SIZE="15"/>
 </node>
-<node CREATED="1365522662245" ID="ID_1860106179" LINK="Vendini_Constants::VENDINI_URL" MODIFIED="1365522701029">
+<node CREATED="1365522662245" ID="ID_1860106179" LINK="Vendini_Constants::VENDINI_URL" MODIFIED="1429113167389">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -267,6 +213,7 @@
     </p>
   </body>
 </html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1382641554621" ID="ID_1919255911" MODIFIED="1382641616729">
 <richcontent TYPE="NODE"><html>
@@ -319,17 +266,79 @@
 <node CREATED="1385462352053" ID="ID_592665931" LINK="http://docs.sublimetext.info/en/latest/reference/key_bindings.html" MODIFIED="1420818750940" TEXT="user keybinding">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-</node>
-<node CREATED="1358849060871" FOLDED="true" ID="ID_1548680707" MODIFIED="1420818754843" POSITION="left" TEXT="learn">
+<node CREATED="1353338690297" FOLDED="true" ID="ID_1889510088" LINK="http://www.sublimetext.com/docs/2/projects.html" MODIFIED="1429115338600" TEXT="projects">
 <font NAME="SansSerif" SIZE="12"/>
-<node COLOR="#009999" CREATED="1358849063683" ID="ID_269352641" LINK="http://net.tutsplus.com/tutorials/tools-and-tips/sublime-text-2-tips-and-tricks/" MODIFIED="1358849073236" TEXT="nettuts">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1429115274148" ID="ID_1882718279" MODIFIED="1429115274148" TEXT="">
+<node CREATED="1378980254553" ID="ID_1209766017" MODIFIED="1378980316184">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      // simple project config
+    </p>
+    <p>
+      <font color="#0033ff">{ </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;"folders": </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;[ </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;{ </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;"follow_symlinks": true, </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;"path": "api-tests" </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;} </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;], </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;"settings": </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;{ </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;"tab_size": 2, </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#xa0;&#xa0;&#xa0;&#xa0;} </font>
+    </p>
+    <p>
+      <font color="#0033ff">} </font>
+    </p>
+  </body>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1363106137025" FOLDED="true" ID="ID_1688653347" MODIFIED="1420818760747" POSITION="left" TEXT="build system">
+<node CREATED="1429115261738" FOLDED="true" ID="ID_550017811" LINK="http://stackoverflow.com/questions/11744365/removing-projects-in-sublime-text-2-and-3" MODIFIED="1429115337788" TEXT="remove recent">
+<node CREATED="1429115283066" ID="ID_701425358" MODIFIED="1429115318042" TEXT="go to Session.sublime_session"/>
+<node CREATED="1429115321492" ID="ID_481131434" MODIFIED="1429115333467" TEXT="and find string with recent_workspace"/>
+</node>
+</node>
+<node CREATED="1429115257284" ID="ID_1141877320" MODIFIED="1429115260252" TEXT="tips"/>
+</node>
+<node CREATED="1358849060871" FOLDED="true" ID="ID_1548680707" MODIFIED="1429115255589" POSITION="left" TEXT="learn">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#009999" CREATED="1358849063683" ID="ID_269352641" LINK="http://net.tutsplus.com/tutorials/tools-and-tips/sublime-text-2-tips-and-tricks/" MODIFIED="1429113141787" TEXT="nettuts">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1363106137025" FOLDED="true" ID="ID_1688653347" MODIFIED="1429113161204" POSITION="left" TEXT="build system">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1363106141257" ID="ID_1778669221" LINK="http://docs.sublimetext.info/en/latest/file_processing/build_systems.html" MODIFIED="1363106144444" TEXT="docs"/>
-<node CREATED="1352716591221" FOLDED="true" ID="ID_1252305111" LINK="http://joeybaker.tumblr.com/post/7304011987/how-to-add-php-validation-to-sublime-text-2" MODIFIED="1420818760501" TEXT="validate php">
+<node CREATED="1429113151192" FOLDED="true" ID="ID_919414629" MODIFIED="1429113159977" TEXT="validate">
+<node CREATED="1352716591221" FOLDED="true" ID="ID_1252305111" LINK="http://joeybaker.tumblr.com/post/7304011987/how-to-add-php-validation-to-sublime-text-2" MODIFIED="1429113159787" TEXT="php">
 <font NAME="SansSerif" SIZE="12"/>
 <node COLOR="#0033ff" CREATED="1353670059473" ID="ID_218911513" MODIFIED="1353670170584">
 <richcontent TYPE="NODE"><html>
@@ -354,6 +363,7 @@
     </p>
   </body>
 </html></richcontent>
+</node>
 </node>
 </node>
 </node>
--- OS/Cross/vim.mm
+++ OS/Cross/vim.mm
@@ -1,7 +1,7 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#990099" CREATED="1297692830782" ID="ID_1675629856" LINK="../Crossplatform.mm" MODIFIED="1351777232291" TEXT="vim">
-<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#990099" CREATED="1297692830782" ID="ID_1675629856" LINK="../Crossplatform.mm" MODIFIED="1429113082767" TEXT="vim">
+<font NAME="SansSerif" SIZE="30"/>
 <node CREATED="1297692832611" FOLDED="true" ID="ID_963268630" MODIFIED="1364488814316" POSITION="left" TEXT="fork">
 <node CREATED="1297692838824" ID="ID_894392305" LINK="https://github.com/carlhuda/janus" MODIFIED="1297692843016" TEXT="Janus"/>
 <node CREATED="1364488594322" FOLDED="true" ID="ID_408990398" MODIFIED="1364488814116" TEXT="browser">
@@ -23,7 +23,7 @@
 <node COLOR="#0033ff" CREATED="1304072329796" ID="ID_21523396" MODIFIED="1310045254652" TEXT="mv ~/.exrc ~/.vimrc "/>
 </node>
 </node>
-<node CREATED="1310398086417" ID="ID_1153701436" MODIFIED="1357753416677" POSITION="left" TEXT="tutorial">
+<node CREATED="1310398086417" FOLDED="true" ID="ID_1153701436" MODIFIED="1429113093519" POSITION="left" TEXT="tutorial">
 <node CREATED="1364487944576" ID="ID_1894261179" LINK="http://net.tutsplus.com/articles/web-roundups/25-vim-tutorials-screencasts-and-resources/" MODIFIED="1364487962537">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -31,20 +31,19 @@
   </head>
   <body>
     <p>
-      <b><font color="#009999">nettuts</font></b>&#160;12 cool info
+      <b><font color="#009999">nettuts</font></b>&#xa0;12 cool info
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1311076066199" FOLDED="true" ID="ID_1137717804" MODIFIED="1357753403533" POSITION="left" TEXT="editing">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1311076070262" FOLDED="true" ID="ID_427727716" MODIFIED="1357753371899" TEXT="commenting">
+<node CREATED="1311076066199" FOLDED="true" ID="ID_1137717804" MODIFIED="1429113103996" POSITION="left" TEXT="editing">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1311076070262" FOLDED="true" ID="ID_427727716" MODIFIED="1429113096834" TEXT="commenting">
 <node CREATED="1311076074462" ID="ID_913033183" LINK="http://eli.thegreenplace.net/2010/08/25/commenting-out-a-block-of-code-with-vim/" MODIFIED="1311076092053" TEXT="tip1"/>
 </node>
-<node CREATED="1355398976420" FOLDED="true" ID="ID_977643914" MODIFIED="1357753402745" TEXT="saving">
-<node COLOR="#0033ff" CREATED="1355398983568" ID="ID_1501735266" MODIFIED="1357753392149">
+<node CREATED="1355398976420" FOLDED="true" ID="ID_977643914" MODIFIED="1429113103740" TEXT="saving">
+<node COLOR="#0033ff" CREATED="1355398983568" ID="ID_1501735266" MODIFIED="1429113102381">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -58,20 +57,21 @@
     </p>
   </body>
 </html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#0033ff" CREATED="1355398992961" ID="ID_1374107260" MODIFIED="1357753243072" TEXT=":w">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#0033ff" CREATED="1355398992961" ID="ID_1374107260" MODIFIED="1429113099864" TEXT=":w">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </node>
-<node CREATED="1332760203082" FOLDED="true" ID="ID_1786211631" MODIFIED="1357753357066" POSITION="left" TEXT="Diff">
-<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1332760203082" FOLDED="true" ID="ID_1786211631" MODIFIED="1429113085752" POSITION="left" TEXT="Diff">
+<font NAME="SansSerif" SIZE="15"/>
 <node CREATED="1332760210574" ID="ID_136649349" LINK="http://www.debianadmin.com/vimdiff-edit-two-or-three-versions-of-a-file-with-vim-and-show-differences.html" MODIFIED="1332760811795" TEXT="vimdiff : site 1"/>
 <node CREATED="1332760213065" ID="ID_186213483" LINK="http://andrejk.blogspot.com/2008/04/vimdiff-howto.html" MODIFIED="1332760808695" TEXT="vimdiff : site 2"/>
 <node CREATED="1332760760362" ID="ID_1786961035" LINK="http://blog.tplus1.com/index.php/2007/08/29/how-to-use-vimdiff-as-the-subversion-diff-tool/" MODIFIED="1332760795593" TEXT="site3 : using vim for subversion"/>
 </node>
-<node CREATED="1322655268811" FOLDED="true" ID="ID_1306109549" MODIFIED="1357753360884" POSITION="left" TEXT="tip">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1322655268811" FOLDED="true" ID="ID_1306109549" MODIFIED="1429113090518" POSITION="left" TEXT="tips">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1356452292583" ID="ID_1729038372" MODIFIED="1356455055259" TEXT="remove all lines">
 <node CREATED="1356452296730" ID="ID_795901926" MODIFIED="1356452319073">
 <richcontent TYPE="NODE"><html>
@@ -80,7 +80,7 @@
   </head>
   <body>
     <p>
-      <font color="#0033ff">gg</font>&#160;and then <font color="#0033ff">dG</font>
+      <font color="#0033ff">gg</font>&#xa0;and then <font color="#0033ff">dG</font>
     </p>
   </body>
 </html></richcontent>
@@ -90,7 +90,7 @@
 <node CREATED="1322655276139" ID="ID_144269626" LINK="http://www.scottmurphy.info/cygwin-windows-vi-vim-backspace-arrow-keys-doesnt-do-not-work-right" MODIFIED="1356452334457" TEXT="work properly"/>
 </node>
 </node>
-<node CREATED="1310044236747" FOLDED="true" ID="ID_424960842" MODIFIED="1357753277072" POSITION="right" TEXT="Tabulation">
+<node CREATED="1310044236747" FOLDED="true" ID="ID_424960842" MODIFIED="1429113115269" POSITION="right" TEXT="Tabulation">
 <node CREATED="1310044404621" ID="ID_1292241249" MODIFIED="1351787744869">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -98,34 +98,34 @@
   </head>
   <body>
     <p>
-      <b>tabstop</b>&#160;Set tabstop to tell vim how many columns a tab counts for.
+      <b>tabstop</b>&#xa0;Set tabstop to tell vim how many columns a tab counts for.
     </p>
     <p>
-      &#160;&#160;- Linux kernel code expects each tab to be eight columns wide.
+      &#xa0;&#xa0;- Linux kernel code expects each tab to be eight columns wide.
     </p>
     <p>
-      &#160;&#160;- Visual Studio expects each tab to be four columns wide.
+      &#xa0;&#xa0;- Visual Studio expects each tab to be four columns wide.
     </p>
     <p>
-      This is the only command here that will affect how existing text displays.&#160;&#160;
-    </p>
-    <p>
-      
-    </p>
-    <p>
-      <b>expandtab</b>&#160;When expandtab is set, hitting Tab in insert mode will produce the appropriate number of spaces.&#160;&#160;
+      This is the only command here that will affect how existing text displays.&#xa0;&#xa0;
     </p>
     <p>
       
     </p>
     <p>
-      <b>shiftwidth</b>&#160;Set shiftwidth to control how many columns text is indented with the reindent operations (&lt;&lt; and &gt;&gt;) and automatic C-style indentation.&#160;&#160;
+      <b>expandtab</b>&#xa0;When expandtab is set, hitting Tab in insert mode will produce the appropriate number of spaces.&#xa0;&#xa0;
     </p>
     <p>
       
     </p>
     <p>
-      <b>softtabstop</b>&#160;Set softtabstop to control how many columns vim uses when you hit Tab in insert mode. If softtabstop is less than <i><b>tabstop</b></i>&#160; and <i><b>expandtab</b></i>&#160;is not set, vim will use a combination of tabs and spaces to make up the desired spacing. If <b>softtabstop</b>&#160;equals <i><b>tabstop</b></i>&#160; and <i><b>expandtab</b></i>&#160;is not set, vim will always use tabs. When expandtab is set, vim will always use the appropriate number of spaces.
+      <b>shiftwidth</b>&#xa0;Set shiftwidth to control how many columns text is indented with the reindent operations (&lt;&lt; and &gt;&gt;) and automatic C-style indentation.&#xa0;&#xa0;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <b>softtabstop</b>&#xa0;Set softtabstop to control how many columns vim uses when you hit Tab in insert mode. If softtabstop is less than <i><b>tabstop</b></i>&#xa0; and <i><b>expandtab</b></i>&#xa0;is not set, vim will use a combination of tabs and spaces to make up the desired spacing. If <b>softtabstop</b>&#xa0;equals <i><b>tabstop</b></i>&#xa0; and <i><b>expandtab</b></i>&#xa0;is not set, vim will always use tabs. When expandtab is set, vim will always use the appropriate number of spaces.
     </p>
   </body>
 </html></richcontent>
@@ -135,7 +135,7 @@
 <node COLOR="#0033ff" CREATED="1310044425146" ID="ID_1734211713" MODIFIED="1310045250148" TEXT=":set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab "/>
 <node COLOR="#0033ff" CREATED="1351699979211" ID="ID_1598390074" MODIFIED="1351699984900" TEXT=":set ts=2"/>
 </node>
-<node CREATED="1310469536627" FOLDED="true" ID="ID_1384056861" MODIFIED="1357753292399" POSITION="right" TEXT="Whitespaces">
+<node CREATED="1310469536627" FOLDED="true" ID="ID_1384056861" MODIFIED="1429113112559" POSITION="right" TEXT="Whitespaces">
 <node CREATED="1310469558569" ID="ID_1083347438" MODIFIED="1310469561224" TEXT="show">
 <node COLOR="#0033ff" CREATED="1310469541075" ID="ID_1033453999" MODIFIED="1310469566175" TEXT=":set list"/>
 </node>
@@ -149,18 +149,18 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1310469586636" FOLDED="true" ID="ID_192037154" MODIFIED="1357753297447" POSITION="right" TEXT="bookmarks">
-<node CREATED="1310469591868" ID="ID_516888858" MODIFIED="1310469594996" TEXT="set">
+<node CREATED="1310469586636" FOLDED="true" ID="ID_192037154" MODIFIED="1429113109575" POSITION="right" TEXT="bookmarks">
+<node CREATED="1310469591868" FOLDED="true" ID="ID_516888858" MODIFIED="1429113108017" TEXT="set">
 <node COLOR="#0033ff" CREATED="1310469595803" ID="ID_1652029897" MODIFIED="1310469598915" TEXT="ma">
 <node CREATED="1310469607402" ID="ID_1895776266" MODIFIED="1310469619137" TEXT="where a is bookmark name"/>
 </node>
 </node>
-<node CREATED="1310469603778" ID="ID_1995553514" MODIFIED="1310469605867" TEXT="goto">
+<node CREATED="1310469603778" FOLDED="true" ID="ID_1995553514" MODIFIED="1429113107752" TEXT="goto">
 <node COLOR="#0033ff" CREATED="1310469624623" ID="ID_1016413642" MODIFIED="1310469634870" TEXT="`a">
 <node CREATED="1310469628031" ID="ID_1112053416" MODIFIED="1310469632216" TEXT="a - bookmark name"/>
 </node>
 </node>
-<node CREATED="1310469642733" ID="ID_35240598" MODIFIED="1310469644606" TEXT="view all">
+<node CREATED="1310469642733" FOLDED="true" ID="ID_35240598" MODIFIED="1429113108565" TEXT="view all">
 <node COLOR="#0033ff" CREATED="1310469645549" ID="ID_931637146" MODIFIED="1310469685785" TEXT=":marks"/>
 </node>
 </node>
@@ -187,8 +187,8 @@
 </node>
 </node>
 </node>
-<node CREATED="1310474111041" FOLDED="true" ID="ID_1803924059" MODIFIED="1357753350959" POSITION="right" TEXT="Tabs">
-<node CREATED="1310474507020" ID="ID_1350974801" MODIFIED="1310474510886" TEXT="goto tab">
+<node CREATED="1310474111041" FOLDED="true" ID="ID_1803924059" MODIFIED="1429113124799" POSITION="right" TEXT="Tabs">
+<node CREATED="1310474507020" ID="ID_1350974801" MODIFIED="1429113121776" TEXT="goto tab">
 <node COLOR="#0033ff" CREATED="1310474511707" ID="ID_1479626322" MODIFIED="1310474528193" TEXT="5gt">
 <node CREATED="1310474529288" ID="ID_1726641561" MODIFIED="1310474534427" TEXT="5 - number of tab"/>
 </node>
@@ -196,7 +196,7 @@
 <node CREATED="1310474622493" ID="ID_1575822056" MODIFIED="1310474635117" TEXT="5 - number of tab"/>
 </node>
 </node>
-<node CREATED="1310474112696" ID="ID_683576352" MODIFIED="1310474652390" TEXT="links">
+<node CREATED="1310474112696" FOLDED="true" ID="ID_683576352" MODIFIED="1429113123867" TEXT="links">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1310474118439" ID="ID_1675762463" LINK="http://vim.wikia.com/wiki/Using_tab_pages" MODIFIED="1310474121922" TEXT="wikia"/>
 <node CREATED="1310474656528" ID="ID_108652882" LINK="http://stackoverflow.com/questions/2005214/switching-to-a-particular-tab-in-vim" MODIFIED="1310474673098" TEXT="stackoverflow"/>
