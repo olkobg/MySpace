761d3eb643a3fed1c4cbadf9118fc9378778b3a9
Oleh Demyanyuk <olkobg@gmail.com>
2015-05-25 22:46:26 +0300

    Updated wordpress map version

--- PHP/CMS/wordpress.mm
+++ PHP/CMS/wordpress.mm
@@ -1,13 +1,13 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#006633" CREATED="1291284864734" ID="ID_995121861" LINK="../PHP.mm" MODIFIED="1355995414525" TEXT="wp">
-<font NAME="SansSerif" SIZE="18"/>
-<node CREATED="1355995349058" ID="ID_906112614" LINK="http://wordpresso.org/" MODIFIED="1355995381431" POSITION="right" TEXT="wordpresso">
+<node COLOR="#006633" CREATED="1291284864734" ID="ID_995121861" LINK="../PHP.mm" MODIFIED="1431884865456" TEXT="wordpress">
+<font NAME="SansSerif" SIZE="27"/>
+<node CREATED="1355995349058" FOLDED="true" ID="ID_906112614" LINK="http://wordpresso.org/" MODIFIED="1431884869023" POSITION="right" TEXT="wordpresso">
 <node CREATED="1355995391192" ID="ID_560583564" LINK="http://wordpresso.org/tutorials/kuda-vstavlyat-etot-kod-v-wordpress-obshhaya-struktura-faylov-temyi/" MODIFIED="1355995400106" TEXT="theme files"/>
 <node CREATED="1355995401069" ID="ID_37526855" LINK="http://wordpresso.org/tutorials/kuda-vstavlyat-etot-kod-v-wordpress-obshhaya-struktura-faylov-temyi/" MODIFIED="1355995411501" TEXT="social nets"/>
 </node>
-<node CREATED="1368127321923" ID="ID_417312875" MODIFIED="1368127323500" POSITION="right" TEXT="tips">
-<node CREATED="1368127324681" ID="ID_105103076" MODIFIED="1368127325464" TEXT="db">
+<node CREATED="1368127321923" FOLDED="true" ID="ID_417312875" MODIFIED="1431884889626" POSITION="right" TEXT="tips">
+<node CREATED="1368127324681" FOLDED="true" ID="ID_105103076" MODIFIED="1431884871814" TEXT="db">
 <node CREATED="1368127326425" ID="ID_667694416" MODIFIED="1368127419876">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -15,28 +15,27 @@
   </head>
   <body>
     <p>
-      --&#160;&#160;attachment look into <b>posts</b>&#160;table with <b>post_type</b>&#160; = '<b>attachment</b>'
+      --&#xa0;&#xa0;attachment look into <b>posts</b>&#xa0;table with <b>post_type</b>&#xa0; = '<b>attachment</b>'
     </p>
     <p>
-      <font color="#0033ff">SELECT * FROM <b>wp_posts</b>&#160;WHERE <b>post_type = 'attachment'</b></font>
+      <font color="#0033ff">SELECT * FROM <b>wp_posts</b>&#xa0;WHERE <b>post_type = 'attachment'</b></font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1367419824095" ID="ID_1090806974" MODIFIED="1367419881673" POSITION="left" TEXT="config">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1367419828814" ID="ID_1829088810" MODIFIED="1367419832964" TEXT="change site url">
-<node COLOR="#009999" CREATED="1367419833911" ID="ID_50442241" LINK="v" MODIFIED="1367419878155" TEXT="warpconduit">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1367419824095" FOLDED="true" ID="ID_1090806974" MODIFIED="1431884887002" POSITION="left" TEXT="config">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1367419828814" FOLDED="true" ID="ID_1829088810" MODIFIED="1431884885383" TEXT="change site url">
+<node COLOR="#009999" CREATED="1367419833911" ID="ID_50442241" LINK="v" MODIFIED="1431884881778" TEXT="warpconduit">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#009999" CREATED="1367419857304" ID="ID_744451692" LINK="http://www.inmotionhosting.com/support/website/wordpress/wordpress-changing-the-site-url-and-home-settings" MODIFIED="1367419878157" TEXT="inmotion hosting">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#009999" CREATED="1367419857304" ID="ID_744451692" LINK="http://www.inmotionhosting.com/support/website/wordpress/wordpress-changing-the-site-url-and-home-settings" MODIFIED="1431884881779" TEXT="inmotion hosting">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#009999" CREATED="1367419869212" ID="ID_1387616713" LINK="http://codex.wordpress.org/Changing_The_Site_URL" MODIFIED="1367419878157" TEXT="wp codex">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#009999" CREATED="1367419869212" ID="ID_1387616713" LINK="http://codex.wordpress.org/Changing_The_Site_URL" MODIFIED="1431884881779" TEXT="wp codex">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1368127040339" ID="ID_1183024639" MODIFIED="1368127042724" TEXT="add "/>
 </node>
