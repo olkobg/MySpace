a3a3021869a4888d6845394edfc80628902ed237
Oleh Demyanyuk <olkobg@gmail.com>
2015-05-26 23:53:15 +0300

    Updated style for sql/postgress maps

--- SQL/postgres/psql.mm
+++ SQL/postgres/psql.mm
@@ -2,11 +2,11 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#0033ff" CREATED="1428579399847" ID="ID_489100025" LINK="../postgres.mm" MODIFIED="1428579510236" TEXT="psql">
 <font NAME="SansSerif" SIZE="27"/>
-<node CREATED="1428579448361" ID="ID_77974947" MODIFIED="1428579448361" POSITION="right" TEXT=""/>
 <node COLOR="#0033ff" CREATED="1428579457973" ID="ID_628088935" MODIFIED="1428579476500" POSITION="left" TEXT="\d">
 <node COLOR="#0033ff" CREATED="1418042889196" ID="ID_1495855500" MODIFIED="1428579474267" TEXT="\du - list users"/>
 </node>
 <node COLOR="#0033ff" CREATED="1428579478339" ID="ID_779312793" MODIFIED="1428579504939" POSITION="left" TEXT="\l - list database"/>
 <node COLOR="#0033ff" CREATED="1428579491218" ID="ID_976421749" MODIFIED="1428579504938" POSITION="left" TEXT="\c - connect to database"/>
+<node COLOR="#0033ff" CREATED="1432582846486" ID="ID_965748657" LINK="http://www.postgresql.org/" MODIFIED="1432582850969" POSITION="right" TEXT="postgresql.org"/>
 </node>
 </map>
