7aeae34e901805b2d8917d3795ebf6890523a04e
Oleh Demyanyuk <olkobg@gmail.com>
2015-05-26 23:54:49 +0300

    Updated style for Index map and added meta info for html

--- Web/HTML.mm
+++ Web/HTML.mm
@@ -1,57 +1,81 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#669900" CREATED="1289984608987" ID="ID_723414545" LINK="../Index.mm" MODIFIED="1422177602347" TEXT="HTML">
+<node COLOR="#669900" CREATED="1289984608987" ID="ID_723414545" LINK="../Index.mm" MODIFIED="1430995724724" TEXT="HTML">
 <font NAME="SansSerif" SIZE="29"/>
-<node CREATED="1297155159356" FOLDED="true" ID="ID_293946390" MODIFIED="1422613362004" POSITION="right" TEXT="3D">
-<node CREATED="1297155285293" ID="ID_683827622" MODIFIED="1297155293437" TEXT="JS">
-<node CREATED="1297155164157" ID="ID_945822937" MODIFIED="1297155166129" TEXT="WebGl">
+<node CREATED="1297155159356" FOLDED="true" ID="ID_293946390" MODIFIED="1431012996245" POSITION="right" TEXT="3D">
+<node CREATED="1297155285293" FOLDED="true" ID="ID_683827622" MODIFIED="1431012995940" TEXT="JS">
+<node CREATED="1297155164157" FOLDED="true" ID="ID_945822937" MODIFIED="1431012995372" TEXT="WebGl">
 <node CREATED="1297155188970" ID="ID_1374482359" LINK="http://en.wikipedia.org/wiki/WebGL" MODIFIED="1297155194357" TEXT="wiki"/>
 <node CREATED="1297155227705" ID="ID_931387873" LINK="http://learningwebgl.com/blog/?page_id=1217" MODIFIED="1297155231958" TEXT="lessons"/>
 </node>
 </node>
 <node CREATED="1297155166643" ID="ID_585398437" LINK="http://unity3d.com/" MODIFIED="1297155209098" TEXT="unity3D"/>
 </node>
-<node CREATED="1307013064378" FOLDED="true" ID="ID_259503846" MODIFIED="1422613360612" POSITION="right" TEXT="WebFonts">
+<node CREATED="1307013064378" FOLDED="true" ID="ID_259503846" MODIFIED="1431012997672" POSITION="right" TEXT="WebFonts">
 <node CREATED="1307013071073" ID="ID_1272693283" LINK="http://code.google.com/apis/webfonts/" MODIFIED="1307013075046" TEXT="google"/>
 <node CREATED="1329495147392" ID="ID_338958224" LINK="http://sixrevisions.com/css/font-face-guide/" MODIFIED="1329495152902" TEXT="guide "/>
 </node>
-<node CREATED="1363001593353" FOLDED="true" ID="ID_1265772728" MODIFIED="1422613367495" POSITION="right" TEXT="css">
-<node CREATED="1363001598666" ID="ID_443450653" LINK="http://www.dynamicdrive.com/style/csslibrary/item/css_triangle_arrow_divs/" MODIFIED="1363001723955">
+<node CREATED="1363001593353" FOLDED="true" ID="ID_1265772728" MODIFIED="1431013010626" POSITION="right" TEXT="css">
+<node CREATED="1363001598666" ID="ID_443450653" LINK="http://www.dynamicdrive.com/style/csslibrary/item/css_triangle_arrow_divs/" MODIFIED="1431013009403">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <font color="#009999"><b>dynamic drive</b></font>&#xa0;create tirangels
+      <font color="#009999">dynamic drive</font>&#160;create tirangels
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1297155298254" ID="ID_751982683" MODIFIED="1422177654588" POSITION="left" TEXT="script">
+<node COLOR="#0033ff" CREATED="1297155298254" FOLDED="true" ID="ID_751982683" MODIFIED="1431013013922" POSITION="left" TEXT="script">
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1297155302594" ID="ID_1348269822" LINK="../JavaScript/JavaScript.mm" MODIFIED="1297155325275" TEXT="JavaScript">
+<node CREATED="1297155302594" FOLDED="true" ID="ID_1348269822" LINK="../JavaScript/JavaScript.mm" MODIFIED="1431013013663" TEXT="JavaScript">
 <node CREATED="1307017640234" ID="ID_1749666599" LINK="http://code.google.com/apis/libraries/devguide.html" MODIFIED="1422177644763" TEXT="google hosted libs"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1333449983322" ID="ID_1859611277" MODIFIED="1422177647811" POSITION="left" TEXT="5">
-<font NAME="SansSerif" SIZE="25"/>
-<node CREATED="1357727196578" ID="ID_77583901" MODIFIED="1357727206082" TEXT="learn">
-<node COLOR="#009999" CREATED="1357719276896" ID="ID_605188182" LINK="http://diveintohtml5.info/" MODIFIED="1357727213676" TEXT="dive into html5">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-</node>
-<node CREATED="1362431798832" ID="ID_1676050199" LINK="http://collegegfx.com/15-useful-html5-tutorials-and-cheat-sheets" MODIFIED="1362431830630">
+<node COLOR="#0033ff" CREATED="1430995724649" FOLDED="true" ID="ID_144216709" MODIFIED="1431012980701" POSITION="left" TEXT="head">
+<node COLOR="#0033ff" CREATED="1430995719113" FOLDED="true" ID="ID_494400543" MODIFIED="1431012980430" TEXT="meta">
+<node CREATED="1430995739495" ID="ID_1906142220" MODIFIED="1430995758634">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <font color="#009999"><b>collegegfx</b></font>&#xa0;html tutors
+      viewport
+    </p>
+    <p>
+      <font color="#0000ff">&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width&quot;&gt;</font>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1430995735696" ID="ID_521167537" MODIFIED="1430995737758" POSITION="left" TEXT="body"/>
+<node COLOR="#0033ff" CREATED="1333449983322" FOLDED="true" ID="ID_1859611277" MODIFIED="1431012977545" POSITION="left" TEXT="5">
+<font NAME="SansSerif" SIZE="25"/>
+<node CREATED="1357727196578" FOLDED="true" ID="ID_77583901" MODIFIED="1431012977288" TEXT="learn">
+<node COLOR="#009999" CREATED="1357719276896" ID="ID_605188182" LINK="http://diveintohtml5.info/" MODIFIED="1431012959562" TEXT="dive into html5">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1362431798832" ID="ID_1676050199" LINK="http://collegegfx.com/15-useful-html5-tutorials-and-cheat-sheets" MODIFIED="1431012975424">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#009999">collegegfx</font>&#160;html tutors
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </node>
