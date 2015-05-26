037bf330b31743a5b3332bbc9298c85019290655
Oleh Demyanyuk <olkobg@gmail.com>
2015-05-26 23:49:27 +0300

    Added capybara-screenshot gem info

---
+++ Ruby&Rails/gems/capybara-screenshot.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#cc3300" CREATED="1432572907509" ID="ID_392629024" LINK="../Gems.mm" MODIFIED="1432581262152" TEXT="capybara-screenshot">
+<font NAME="SansSerif" SIZE="23"/>
+<node COLOR="#0033ff" CREATED="1432577647236" ID="ID_1324850310" MODIFIED="1432577660097" POSITION="left" TEXT="Capybara::Screenshot.screenshot_and_save_page "/>
+<node COLOR="#0033ff" CREATED="1432577656242" ID="ID_1151255220" MODIFIED="1432577658648" POSITION="left" TEXT="Capybara::Screenshot.screenshot_and_open_image"/>
+<node COLOR="#0033ff" CREATED="1432577780877" ID="ID_1707906235" LINK="https://github.com/mattheworiordan/capybara-screenshot" MODIFIED="1432577783256" POSITION="right" TEXT="https://github.com/mattheworiordan/capybara-screenshot"/>
+</node>
+</map>
--- Ruby&Rails/gems/capybara.mm
+++ Ruby&Rails/gems/capybara.mm
@@ -1,10 +1,11 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#990000" CREATED="1419367386094" ID="ID_1483534701" LINK="../Gems.mm" MODIFIED="1430336988810" TEXT="capybara">
+<node COLOR="#990000" CREATED="1419367386094" ID="ID_1483534701" LINK="../Gems.mm" MODIFIED="1432572823086" TEXT="capybara">
 <font NAME="SansSerif" SIZE="28"/>
 <node COLOR="#0033ff" CREATED="1430337013184" ID="ID_2400714" LINK="https://github.com/jnicklas/capybara" MODIFIED="1430337016469" POSITION="right" TEXT="https://github.com/jnicklas/capybara"/>
+<node COLOR="#0033ff" CREATED="1432572907509" ID="ID_893945121" LINK="https://github.com/mattheworiordan/capybara-screenshot" MODIFIED="1432572924713" POSITION="right" TEXT="capybara-screenshot"/>
 <node CREATED="1420547611128" ID="ID_413190233" LINK="https://gist.github.com/zhengjia/428105" MODIFIED="1420547615132" POSITION="right" TEXT="cheatsheet"/>
-<node CREATED="1420816103240" FOLDED="true" ID="ID_119213977" MODIFIED="1430336985968" POSITION="right" TEXT="webdrivers">
+<node CREATED="1420816103240" FOLDED="true" ID="ID_119213977" MODIFIED="1432582755724" POSITION="right" TEXT="webdrivers">
 <node CREATED="1420816090401" ID="ID_1376411042" LINK="https://github.com/teampoltergeist/poltergeist" MODIFIED="1420816151087">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -18,11 +19,11 @@
 </html></richcontent>
 </node>
 <node CREATED="1420816153239" ID="ID_36892055" MODIFIED="1420816160786" TEXT="selenium web driver"/>
-<node COLOR="#0033ff" CREATED="1421274324833" FOLDED="true" ID="ID_1038613521" LINK="https://github.com/thoughtbot/capybara-webkit" MODIFIED="1430336707053" TEXT="capybara-webkit">
+<node COLOR="#0033ff" CREATED="1421274324833" FOLDED="true" ID="ID_1038613521" LINK="https://github.com/thoughtbot/capybara-webkit" MODIFIED="1432582754231" TEXT="capybara-webkit">
 <node COLOR="#0033ff" CREATED="1421274335863" ID="ID_700019120" LINK="https://github.com/leonid-shevtsov/headless" MODIFIED="1421274353822" TEXT="headless"/>
 </node>
 </node>
-<node CREATED="1420464410055" FOLDED="true" ID="ID_686499212" LINK="http://stackoverflow.com/questions/5153550/capybara-assert-attributes-of-an-element" MODIFIED="1430336972292" POSITION="left" TEXT="css \ xpath">
+<node CREATED="1420464410055" FOLDED="true" ID="ID_686499212" LINK="http://stackoverflow.com/questions/5153550/capybara-assert-attributes-of-an-element" MODIFIED="1432582738417" POSITION="left" TEXT="css \ xpath">
 <node CREATED="1420534865395" ID="ID_626892246" MODIFIED="1430336894582">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -63,11 +64,10 @@
       <font color="#0000ff">find(:xpath, &quot;//a[@class='disabled']&quot;).should be_visible</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1420709554919" FOLDED="true" ID="ID_1142036426" LINK="http://stackoverflow.com/questions/12513072/capybara-testing-if-page-has-regular-expression" MODIFIED="1430336972580" POSITION="left" TEXT="match">
+<node COLOR="#0033ff" CREATED="1420709554919" FOLDED="true" ID="ID_1142036426" LINK="http://stackoverflow.com/questions/12513072/capybara-testing-if-page-has-regular-expression" MODIFIED="1432582741847" POSITION="left" TEXT="match">
 <node CREATED="1420709559162" ID="ID_1288666203" MODIFIED="1430336834158">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -99,12 +99,11 @@
       <font color="#0000ff">find(&quot;span.time&quot;).text.should match(/^\d{2}:\d{2}/)</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1430336753051" FOLDED="true" ID="ID_1578409844" MODIFIED="1430341248078" POSITION="left" TEXT="find">
-<node CREATED="1430336755751" FOLDED="true" ID="ID_1484634485" MODIFIED="1430341247854" TEXT="by placeholder">
+<node COLOR="#0033ff" CREATED="1430336753051" FOLDED="true" ID="ID_1578409844" MODIFIED="1432582747063" POSITION="left" TEXT="find">
+<node CREATED="1430336755751" FOLDED="true" ID="ID_1484634485" MODIFIED="1432582746719" TEXT="by placeholder">
 <node COLOR="#0033ff" CREATED="1430336761583" ID="ID_240289390" MODIFIED="1430336851031">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -115,13 +114,12 @@
       find(&quot;input[placeholder='First Name']&quot;).set &quot;value&quot;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 <node CREATED="1420758363139" ID="ID_95244188" LINK="http://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara" MODIFIED="1420758381537" POSITION="left" TEXT="wait for ajax"/>
-<node CREATED="1420553002865" FOLDED="true" ID="ID_1981479091" LINK="http://stackoverflow.com/questions/14346314/multiple-capybara-wait-times" MODIFIED="1430336983524" POSITION="left" TEXT="wait times">
+<node CREATED="1420553002865" FOLDED="true" ID="ID_1981479091" LINK="http://stackoverflow.com/questions/14346314/multiple-capybara-wait-times" MODIFIED="1432582749333" POSITION="left" TEXT="wait times">
 <node CREATED="1420752658721" ID="ID_1558064907" LINK="https://groups.google.com/forum/#!topic/ruby-capybara/LCXCdPaHNe0" MODIFIED="1420752670437" TEXT="wait before conten appear"/>
 </node>
 </node>
