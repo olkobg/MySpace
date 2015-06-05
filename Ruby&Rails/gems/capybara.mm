f463c2a46647a6a79fc3b549a9ab9f4377bb21ed
Oleh Demyanyuk <olkobg@gmail.com>
2015-06-05 23:54:49 +0300

    Added 404 page test example

--- Ruby&Rails/gems/capybara.mm
+++ Ruby&Rails/gems/capybara.mm
@@ -5,7 +5,7 @@
 <node COLOR="#0033ff" CREATED="1430337013184" ID="ID_2400714" LINK="https://github.com/jnicklas/capybara" MODIFIED="1430337016469" POSITION="right" TEXT="https://github.com/jnicklas/capybara"/>
 <node COLOR="#0033ff" CREATED="1432572907509" ID="ID_893945121" LINK="https://github.com/mattheworiordan/capybara-screenshot" MODIFIED="1432572924713" POSITION="right" TEXT="capybara-screenshot"/>
 <node CREATED="1420547611128" ID="ID_413190233" LINK="https://gist.github.com/zhengjia/428105" MODIFIED="1420547615132" POSITION="right" TEXT="cheatsheet"/>
-<node CREATED="1420816103240" FOLDED="true" ID="ID_119213977" MODIFIED="1432582755724" POSITION="right" TEXT="webdrivers">
+<node CREATED="1420816103240" FOLDED="true" ID="ID_119213977" MODIFIED="1433232473454" POSITION="right" TEXT="webdrivers">
 <node CREATED="1420816090401" ID="ID_1376411042" LINK="https://github.com/teampoltergeist/poltergeist" MODIFIED="1420816151087">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -23,7 +23,12 @@
 <node COLOR="#0033ff" CREATED="1421274335863" ID="ID_700019120" LINK="https://github.com/leonid-shevtsov/headless" MODIFIED="1421274353822" TEXT="headless"/>
 </node>
 </node>
-<node CREATED="1420464410055" FOLDED="true" ID="ID_686499212" LINK="http://stackoverflow.com/questions/5153550/capybara-assert-attributes-of-an-element" MODIFIED="1432582738417" POSITION="left" TEXT="css \ xpath">
+<node CREATED="1433232487430" FOLDED="true" ID="ID_1563757293" MODIFIED="1433232520360" POSITION="right" TEXT="info">
+<node CREATED="1433232515246" FOLDED="true" ID="ID_198710022" MODIFIED="1433232520061" TEXT="article">
+<node COLOR="#0033ff" CREATED="1433232489479" ID="ID_1779940487" LINK="http://www.codeitive.com/7izyPVjWVV/feature-testing-with-filepickerio-and-capybara.html" MODIFIED="1433232511883" TEXT="filepicker.io test"/>
+</node>
+</node>
+<node CREATED="1420464410055" FOLDED="true" ID="ID_686499212" LINK="http://stackoverflow.com/questions/5153550/capybara-assert-attributes-of-an-element" MODIFIED="1433232478441" POSITION="left" TEXT="css \ xpath">
 <node CREATED="1420534865395" ID="ID_626892246" MODIFIED="1430336894582">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -67,7 +72,7 @@
 </html></richcontent>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1420709554919" FOLDED="true" ID="ID_1142036426" LINK="http://stackoverflow.com/questions/12513072/capybara-testing-if-page-has-regular-expression" MODIFIED="1432582741847" POSITION="left" TEXT="match">
+<node COLOR="#0033ff" CREATED="1420709554919" FOLDED="true" ID="ID_1142036426" LINK="http://stackoverflow.com/questions/12513072/capybara-testing-if-page-has-regular-expression" MODIFIED="1433232479618" POSITION="left" TEXT="match">
 <node CREATED="1420709559162" ID="ID_1288666203" MODIFIED="1430336834158">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -102,8 +107,8 @@
 </html></richcontent>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1430336753051" FOLDED="true" ID="ID_1578409844" MODIFIED="1432582747063" POSITION="left" TEXT="find">
-<node CREATED="1430336755751" FOLDED="true" ID="ID_1484634485" MODIFIED="1432582746719" TEXT="by placeholder">
+<node COLOR="#0033ff" CREATED="1430336753051" FOLDED="true" ID="ID_1578409844" MODIFIED="1433526903414" POSITION="left" TEXT="find">
+<node CREATED="1430336755751" FOLDED="true" ID="ID_1484634485" MODIFIED="1433232481380" TEXT="by placeholder">
 <node COLOR="#0033ff" CREATED="1430336761583" ID="ID_240289390" MODIFIED="1430336851031">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -119,8 +124,34 @@
 </node>
 </node>
 <node CREATED="1420758363139" ID="ID_95244188" LINK="http://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara" MODIFIED="1420758381537" POSITION="left" TEXT="wait for ajax"/>
-<node CREATED="1420553002865" FOLDED="true" ID="ID_1981479091" LINK="http://stackoverflow.com/questions/14346314/multiple-capybara-wait-times" MODIFIED="1432582749333" POSITION="left" TEXT="wait times">
+<node CREATED="1420553002865" FOLDED="true" ID="ID_1981479091" LINK="http://stackoverflow.com/questions/14346314/multiple-capybara-wait-times" MODIFIED="1433232476194" POSITION="left" TEXT="wait times">
 <node CREATED="1420752658721" ID="ID_1558064907" LINK="https://groups.google.com/forum/#!topic/ruby-capybara/LCXCdPaHNe0" MODIFIED="1420752670437" TEXT="wait before conten appear"/>
+</node>
+<node CREATED="1433526904727" FOLDED="true" ID="ID_1275127514" MODIFIED="1433537544288" POSITION="left" TEXT="expect">
+<node CREATED="1433526907267" FOLDED="true" ID="ID_836596391" MODIFIED="1433537544072" TEXT="404">
+<node CREATED="1433526908923" ID="ID_927933420" MODIFIED="1433526945603">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff">scenario &quot;as an unauthorized user&quot; do </font>
+    </p>
+    <p>
+      <font color="#0000ff">&#160;&#160;visit prospective_users_path </font>
+    </p>
+    <p>
+      <font color="#0000ff">&#160;&#160;expect(page.status_code).to eq(404) </font>
+    </p>
+    <p>
+      <font color="#0000ff">end </font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </map>
