b9595186e9a5da0b3bb7c9b615b51a687c4086fd
Oleh Demyanyuk <olkobg@gmail.com>
2015-07-07 12:09:08 +0300

    Added Hashie and figaro gems info

---
+++ Ruby&Rails/gems/Hashie.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#990000" CREATED="1434736877237" ID="ID_1907444689" LINK="../Gems.mm" MODIFIED="1434736981954" TEXT="Hashie">
+<font NAME="SansSerif" SIZE="27"/>
+<node CREATED="1434736940267" FOLDED="true" ID="ID_1488363373" MODIFIED="1434736981693" POSITION="left" TEXT="Mash">
+<node CREATED="1434736964225" ID="ID_728646081" MODIFIED="1434736979805" TEXT="creates setter and getter from hash"/>
+</node>
+<node COLOR="#0033ff" CREATED="1434736953455" ID="ID_675956896" LINK="https://github.com/intridea/hashie" MODIFIED="1434736956418" POSITION="right" TEXT="https://github.com/intridea/hashie"/>
+</node>
+</map>
---
+++ Ruby&Rails/gems/figaro.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#990000" CREATED="1429865021405" ID="ID_468810988" LINK="../Gems.mm" MODIFIED="1436173923690" TEXT="figaro">
+<font NAME="SansSerif" SIZE="24"/>
+<node COLOR="#0033ff" CREATED="1436173940992" ID="ID_1860244518" LINK="https://github.com/laserlemon/figaro" MODIFIED="1436173946664" POSITION="right" TEXT="https://github.com/laserlemon/figaro"/>
+<node COLOR="#0033ff" CREATED="1436174030551" FOLDED="true" ID="ID_409538097" MODIFIED="1436174058202" POSITION="right" TEXT="$">
+<node COLOR="#0033ff" CREATED="1436174033778" ID="ID_1070095973" MODIFIED="1436174046473" TEXT="$ rails generate figaro:install"/>
+</node>
+<node COLOR="#0033ff" CREATED="1436173951084" ID="ID_153825758" MODIFIED="1436185336343" POSITION="left" TEXT="application.yml">
+<node COLOR="#0033ff" CREATED="1436173957220" ID="ID_1750323170" MODIFIED="1436173997940">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      HELLO: world
+    </p>
+    <p>
+      development:
+    </p>
+    <p>
+      &#160;&#160;HELLO: developers
+    </p>
+    <p>
+      production:
+    </p>
+    <p>
+      &#160;&#160;HELLO: users
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#0033ff" CREATED="1436174014178" ID="ID_1241836179" MODIFIED="1436174018121" TEXT="Figaro.env.gmail_username"/>
+<node COLOR="#0033ff" CREATED="1436174025026" ID="ID_1510034591" MODIFIED="1436174026761" TEXT="GMAIL_USERNAME: Your_Username"/>
+</node>
+<node CREATED="1436185339226" ID="ID_1765945049" MODIFIED="1436185339226" POSITION="left" TEXT=""/>
+</node>
+</map>
