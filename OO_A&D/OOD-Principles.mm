19caa60e06ebb3ac85ef7a1b0e3bf34b1c4ee143
Oleh Demyanyuk <olkobg@gmail.com>
2015-07-16 15:37:43 +0300

    Converted map and added rest of SOLID principles

--- OO_A&D/OOD-Principles.mm
+++ OO_A&D/OOD-Principles.mm
@@ -1,16 +1,17 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1289984646135" ID="ID_1853198355" LINK="../Index.mm" MODIFIED="1299236835041" TEXT="OOD">
-<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1289984646135" ID="ID_1853198355" LINK="../Index.mm" MODIFIED="1437042366152" TEXT="OOD">
+<font NAME="SansSerif" SIZE="31"/>
 <node CREATED="1296835925807" ID="ID_915433591" MODIFIED="1296835945283" POSITION="left" TEXT="TDD"/>
-<node CREATED="1289984627900" ID="ID_880339721" LINK="Agile.mm" MODIFIED="1315390181695" POSITION="left" TEXT="Agile">
+<node CREATED="1289984627900" FOLDED="true" ID="ID_880339721" LINK="Agile.mm" MODIFIED="1437042362083" POSITION="left" TEXT="Agile">
 <node CREATED="1289984798504" ID="ID_175964393" LINK="Pragmatic-Learning.mm" MODIFIED="1361878375586" TEXT="Pragmaric Learning"/>
 <node CREATED="1289984639926" ID="ID_1753350196" LINK="Scrum.mm" MODIFIED="1299236720888" TEXT="SCRUM"/>
 <node CREATED="1297689878385" ID="ID_275709927" LINK="KanBan.mm" MODIFIED="1299236726650" TEXT="KanBan"/>
 <node CREATED="1296835922393" ID="ID_228552236" MODIFIED="1296835925074" TEXT="XP"/>
 </node>
 <node CREATED="1299228795413" ID="ID_1917404631" LINK="OO-Patterns.mm" MODIFIED="1342441661924" POSITION="left" TEXT="Patterns"/>
-<node CREATED="1299236823267" ID="ID_337803589" MODIFIED="1299236854590" POSITION="right" TEXT="Principles">
+<node CREATED="1299236823267" FOLDED="true" ID="ID_337803589" MODIFIED="1437042506015" POSITION="right" TEXT="Principles">
+<font NAME="SansSerif" SIZE="17"/>
 <node CREATED="1299236830875" ID="ID_445043046" MODIFIED="1299242608771">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -26,61 +27,81 @@
 <node CREATED="1299242601708" ID="ID_417506604" MODIFIED="1299242602641" TEXT="Program to interfaces, not implementations."/>
 <node CREATED="1299242610645" ID="ID_851380906" MODIFIED="1299242611522" TEXT="Favor composition over inheritence."/>
 <node CREATED="1299236856920" ID="ID_1807425320" MODIFIED="1299242965187" TEXT=" Strive for loosely coupled designs between objects that interact"/>
-<node CREATED="1283930953234" ID="ID_1778766102" MODIFIED="1299498701235" TEXT="Common">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1299240942928" ID="ID_1565221215" MODIFIED="1348135628863">
+<node CREATED="1283930953234" FOLDED="true" ID="ID_1778766102" LINK="https://en.wikipedia.org/wiki/SOLID_%28object-oriented_design%29" MODIFIED="1437042505038" TEXT="SOLID">
+<font NAME="SansSerif" SIZE="21"/>
+<node CREATED="1299240942928" FOLDED="true" ID="ID_1565221215" MODIFIED="1437042428949">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b>SRP&#160; </b>&#160;(Single responsibility principle)
+      <b>SRP&#xa0; </b>&#xa0;(Single responsibility principle)
     </p>
   </body>
 </html></richcontent>
+<node CREATED="1437042427075" ID="ID_783364357" MODIFIED="1437042427878" TEXT="a class should have only a single responsibility (i.e. only one potential change in the software&apos;s specification should be able to affect the specification of the class)"/>
 </node>
-<node CREATED="1283930987468" ID="ID_470383395" MODIFIED="1348135632613">
+<node CREATED="1283930987468" FOLDED="true" ID="ID_470383395" MODIFIED="1437042447868">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <font size="3"><b>OCP</b></font>&#160;&#160;(Open - closed principle)
+      <font size="3"><b>OCP</b></font>&#xa0;&#xa0;(Open - closed principle)
     </p>
   </body>
 </html></richcontent>
+<node CREATED="1437042445549" ID="ID_996891124" MODIFIED="1437042446404" TEXT="&#x201c;software entities &#x2026; should be open for extension, but closed for modification.&#x201d;"/>
 </node>
-<node CREATED="1283930974984" ID="ID_1595037506" MODIFIED="1348135636218">
+<node CREATED="1283931003859" FOLDED="true" ID="ID_439271811" MODIFIED="1437042473161">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b>DIP</b>&#160;&#160;(Depency inversion principle)
+      <b>LSP</b>&#xa0;&#xa0; (Liskov substituion pronciple)
     </p>
   </body>
 </html></richcontent>
+<node CREATED="1437042471527" ID="ID_1392933507" MODIFIED="1437042472329" TEXT="&#x201c;objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program.&#x201d; See also design by contract."/>
 </node>
-<node CREATED="1283931003859" ID="ID_439271811" MODIFIED="1348135632609">
+<node CREATED="1437042271596" FOLDED="true" ID="ID_1676081225" MODIFIED="1437042482147">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      <b>LSP</b>&#160;&#160; (Liskov substituion pronciple)
+      <b>ISP </b>- interface segregation principle
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1437042480713" ID="ID_105587044" MODIFIED="1437042481328" TEXT="&#x201c;many client-specific interfaces are better than one general-purpose interface.&#x201d;[8]"/>
+</node>
+<node CREATED="1283930974984" FOLDED="true" ID="ID_1595037506" MODIFIED="1437042498433">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>DIP</b>&#xa0;&#xa0;(Depency inversion principle)
     </p>
   </body>
 </html></richcontent>
+<node CREATED="1437042496837" ID="ID_931036513" MODIFIED="1437042497463" TEXT="one should &#x201c;Depend upon Abstractions. Do not depend upon concretions.&#x201d;[8]"/>
 </node>
 </node>
 </node>
-<node CREATED="1348821359881" ID="ID_361103097" MODIFIED="1348821361931" POSITION="right" TEXT="Tips">
-<node CREATED="1348821362734" ID="ID_1713725614" MODIFIED="1348821376879" TEXT="class">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1348821359881" FOLDED="true" ID="ID_361103097" MODIFIED="1437042395718" POSITION="right" TEXT="info">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1437042388406" FOLDED="true" ID="ID_138865488" MODIFIED="1437042395423" TEXT="glossary">
+<node COLOR="#0033ff" CREATED="1348821362734" FOLDED="true" ID="ID_1713725614" MODIFIED="1437042395136" TEXT="class">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1348821377516" ID="ID_187622606" MODIFIED="1348821479499">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -88,10 +109,11 @@
   </head>
   <body>
     <p>
-      describes what <font color="#0033ff"><b>object</b></font>&#160;<b><i>know</i></b>&#160;and what <font color="#0033ff"><b>object</b></font>&#160;<b><i>does</i></b>
+      describes what <font color="#0033ff"><b>object</b></font>&#xa0;<b><i>know</i></b>&#xa0;and what <font color="#0033ff"><b>object</b></font>&#xa0;<b><i>does</i></b>
     </p>
   </body>
 </html></richcontent>
+</node>
 </node>
 </node>
 </node>
