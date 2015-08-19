f9354ede55854c735b3f3a82891155152a4880b7
Oleh Demyanyuk <olkobg@gmail.com>
2015-08-19 13:01:56 +0300

    Add MacVim info and update vim info

---
+++ OS/MacOS/Macvim.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#006633" CREATED="1439975476408" ID="ID_270936691" LINK="../Cross/vim.mm" MODIFIED="1439975740689" TEXT="MacVim">
+<font NAME="SansSerif" SIZE="25"/>
+<node CREATED="1439975726062" FOLDED="true" ID="ID_670088644" MODIFIED="1439975759306" POSITION="right" TEXT="config">
+<node COLOR="#0033ff" CREATED="1439975742852" ID="ID_1345499039" LINK="http://stackoverflow.com/questions/8483371/set-macvim-default-font" MODIFIED="1439975754910" TEXT="font"/>
+</node>
+</node>
+</map>
