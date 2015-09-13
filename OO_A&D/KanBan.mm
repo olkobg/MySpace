8bf0a07e6ca3f9d6f96b6a80b211008045759c90
Oleh Demyanyuk <olkobg@gmail.com>
2015-09-13 22:29:29 +0300

    Updated KanBan and Scrum to newer version Changed Pragmatic-Learning style

--- OO_A&D/KanBan.mm
+++ OO_A&D/KanBan.mm
@@ -1,9 +1,9 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#0033ff" CREATED="1292430909349" ID="ID_245784441" LINK="OOD-Principles.mm" MODIFIED="1315938695980" TEXT="KanBan">
-<font BOLD="true" NAME="SansSerif" SIZE="20"/>
-<node CREATED="1297689880351" ID="ID_668012743" LINK="http://agilescout.com/best-kanban-tools/" MODIFIED="1297689889249" POSITION="right" TEXT="best web tools">
-<node CREATED="1297689885266" ID="ID_933688061" MODIFIED="1297689885266" TEXT=""/>
+<node COLOR="#0033ff" CREATED="1292430909349" ID="ID_245784441" LINK="OOD-Principles.mm" MODIFIED="1438703126998" TEXT="KanBan">
+<font NAME="SansSerif" SIZE="26"/>
+<node CREATED="1297689880351" ID="ID_668012743" LINK="http://agilescout.com/best-kanban-tools/" MODIFIED="1438703013545" POSITION="right" TEXT="best web tools">
+<font NAME="SansSerif" SIZE="15"/>
 </node>
 </node>
 </map>
