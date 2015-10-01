f8ac9037076a9e1e3a62eb6adf63dc63b5f0b2cd
Oleh Demyanyuk <olkobg@gmail.com>
2015-10-01 11:50:49 +0300

    Add zeus preloader info

---
+++ Ruby&Rails/gems/zeus.mm
@@ -0,0 +1,28 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#990000" CREATED="1443689005344" ID="ID_641613243" LINK="../Rails.mm" MODIFIED="1443689061942" TEXT="zeus">
+<font NAME="SansSerif" SIZE="27"/>
+<node CREATED="1443689088518" FOLDED="true" ID="ID_123689988" MODIFIED="1443689271155" POSITION="right" TEXT="lib">
+<node COLOR="#0033ff" CREATED="1443689091817" ID="ID_1332460633" LINK="https://github.com/sevos/zeus-parallel_tests" MODIFIED="1443689151420" TEXT="zeus-parallel_tests"/>
+</node>
+<node CREATED="1443689082024" FOLDED="true" ID="ID_1121237459" MODIFIED="1443689269405" POSITION="right" TEXT="info">
+<node COLOR="#0033ff" CREATED="1443689201371" ID="ID_1099888009" LINK="http://stackoverflow.com/questions/16220292/how-to-run-rails-console-in-test-environment-with-zeus" MODIFIED="1443689218841" TEXT="How to run test console"/>
+</node>
+<node CREATED="1443689224451" ID="ID_89259431" MODIFIED="1443689265676" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # run console
+    </p>
+    <p>
+      <font color="#0000ff">$ zeus c</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
