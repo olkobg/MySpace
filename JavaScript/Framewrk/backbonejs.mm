3c0b060ff4e6689a3e47683dcbf37fec82ad5e08
Oleh Demyanyuk <olkobg@gmail.com>
2015-10-11 18:22:39 +0300

    Add back bone pramatic web site

--- JavaScript/Framewrk/backbonejs.mm
+++ JavaScript/Framewrk/backbonejs.mm
@@ -2,10 +2,17 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node COLOR="#0033ff" CREATED="1375964463437" ID="ID_96144430" LINK="../JavaScript.mm" MODIFIED="1443266550315" TEXT="backbone.js">
 <font NAME="SansSerif" SIZE="24"/>
-<node CREATED="1443266550263" FOLDED="true" ID="ID_486889011" MODIFIED="1443300546529" POSITION="right" TEXT="info">
-<node COLOR="#0033ff" CREATED="1375964470929" ID="ID_299071080" LINK="http://blog.bigbinary.com/2011/08/18/understanding-bind-and-bindall-in-backbone.html" MODIFIED="1443266549455" TEXT="bind difference"/>
+<node CREATED="1443266550263" FOLDED="true" ID="ID_486889011" MODIFIED="1444576926043" POSITION="right" TEXT="info">
+<node COLOR="#0033ff" CREATED="1375964470929" ID="ID_299071080" LINK="http://blog.bigbinary.com/2011/08/18/understanding-bind-and-bindall-in-backbone.html" MODIFIED="1444576865544" TEXT="bind difference"/>
+<node CREATED="1444576880859" FOLDED="true" ID="ID_1170773987" MODIFIED="1444576924904" TEXT="blogs">
+<node COLOR="#0033ff" CREATED="1444576887490" ID="ID_1101726769" LINK="http://pragmatic-backbone.com/" MODIFIED="1444576891378" TEXT="pragmatic-backbone.com"/>
+<node COLOR="#0033ff" CREATED="1444576887492" ID="ID_90125808" LINK="http://ricostacruz.com/backbone-patterns/" MODIFIED="1444576891371" TEXT="ricostacruz.com &gt; Backbone-patterns"/>
+</node>
+<node CREATED="1444576861429" FOLDED="true" ID="ID_6459414" MODIFIED="1444576924668" TEXT="genral">
 <node CREATED="1443266564111" ID="ID_1454684733" MODIFIED="1443266620766" TEXT="created by Jeremy Ashkenas"/>
 <node CREATED="1443266622267" ID="ID_1449595877" MODIFIED="1443266645045" TEXT="help to create app structure and provide it with models"/>
+</node>
+<node CREATED="1444576854524" ID="ID_1397861661" MODIFIED="1444576855795" TEXT="so"/>
 </node>
 <node COLOR="#0033ff" CREATED="1443266553347" FOLDED="true" ID="ID_1688852000" MODIFIED="1443300536673" POSITION="left" TEXT="Model">
 <node CREATED="1443267030169" ID="ID_151392113" MODIFIED="1443267152283">
@@ -30,8 +37,7 @@
       var todoItem = new TodoItem ( { description: &quot;New Item&quot;, status &quot;incomplete&quot;, id: 1 } )
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node COLOR="#0033ff" CREATED="1443267438369" FOLDED="true" ID="ID_794892907" MODIFIED="1443268166999" TEXT="get">
 <node COLOR="#0033ff" CREATED="1443267446838" ID="ID_1781417737" MODIFIED="1443267469195" TEXT="model.get(&apos;description&apos;)"/>
@@ -56,8 +62,7 @@
       model.url = '/todo'
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1443268143025" FOLDED="true" ID="ID_1291782834" MODIFIED="1443268168779" TEXT="fetch">
@@ -108,8 +113,7 @@
       var todoView = new TodoView( { model: todoItem } );
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
