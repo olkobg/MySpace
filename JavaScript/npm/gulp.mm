2a920eae0289bc83e3b0cf63765adfa300b3c9d5
Oleh Demyanyuk <olkobg@gmail.com>
2015-10-16 23:37:26 +0300

    Add gulp related packages for nodejs

---
+++ JavaScript/npm/gulp.mm
@@ -0,0 +1,64 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1445012399127" ID="ID_826164337" LINK="../npm.mm" MODIFIED="1445012823374" TEXT="gulp">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1445012832935" FOLDED="true" ID="ID_1813381009" MODIFIED="1445027418530" POSITION="left" TEXT="gulpfile.js">
+<node CREATED="1445013268966" ID="ID_1994396847" MODIFIED="1445013332935">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000ff">gulp.task('task-name', [</font>tasks should be run before current task'<font color="#0000ff">]</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#0033ff" CREATED="1445013856063" FOLDED="true" ID="ID_381186071" MODIFIED="1445027416112" TEXT="default">
+<node CREATED="1445013862913" ID="ID_1081655742" MODIFIED="1445013869298" TEXT="is a task name to run first"/>
+</node>
+</node>
+<node CREATED="1445013076485" FOLDED="true" ID="ID_1167774030" MODIFIED="1445027417654" POSITION="left" TEXT="modules">
+<node COLOR="#0033ff" CREATED="1445012401556" FOLDED="true" ID="ID_90698787" MODIFIED="1445013685462" TEXT="gulp-connect">
+<node CREATED="1445013100324" ID="ID_643290692" MODIFIED="1445013105391" TEXT="used to run local dev sever"/>
+<node COLOR="#0033ff" CREATED="1445013665050" ID="ID_565931204" MODIFIED="1445013672179">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;connect.sever({
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;root: ['dist'],
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;port: config.port,
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;base: config.devBaseUrl,
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;livereload: true
+    </p>
+    <p>
+      &#160;&#160;});
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1445012419941" FOLDED="true" ID="ID_1055643015" MODIFIED="1445013114260" TEXT="gulp-open">
+<node CREATED="1445013106471" ID="ID_1817212593" MODIFIED="1445013113462" TEXT="used to open url "/>
+</node>
+<node COLOR="#0033ff" CREATED="1445024908941" FOLDED="true" ID="ID_566025274" MODIFIED="1445024950613" TEXT="gulp-concat">
+<node CREATED="1445024940672" ID="ID_298565306" MODIFIED="1445024948710" TEXT="Concatenates files"/>
+</node>
+<node COLOR="#0033ff" CREATED="1445025968113" FOLDED="true" ID="ID_122155145" MODIFIED="1445025992148" TEXT="gulp-eslint">
+<node CREATED="1445025976868" ID="ID_1180399374" MODIFIED="1445025990894" TEXT="to make eslint service over our files"/>
+</node>
+</node>
+</node>
+</map>
