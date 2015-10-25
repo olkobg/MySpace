ee329cc996879381151b544792b45485a2093359
Oleh Demyanyuk <olkobg@gmail.com>
2015-10-25 15:28:40 +0200

    Add react.js info

---
+++ JavaScript/Framewrk/reactjs.mm
@@ -0,0 +1,54 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1444566531574" ID="ID_1576832072" LINK="../JavaScript.mm" MODIFIED="1445771658794" TEXT="react.js">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1445771739151" FOLDED="true" ID="ID_1780455851" MODIFIED="1445779492646" POSITION="left" TEXT="components">
+<node CREATED="1445772155711" ID="ID_1326073744" MODIFIED="1445779487484">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      // each component require to implement render function
+    </p>
+    <p>
+      <font color="#0033ff">var Home = React.createClass({ </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;render: function() { </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;&#160;&#160;return ( </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;&#160;&#160;&#160;&#160;&lt;div className=&quot;jumbotron&quot;&gt; </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Hello!. </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;&#160;&#160;&#160;&#160;&lt;/div&gt; </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;&#160;&#160;); </font>
+    </p>
+    <p>
+      <font color="#0033ff">&#160;&#160;} </font>
+    </p>
+    <p>
+      <font color="#0033ff">});</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1445771755314" ID="ID_1133848702" MODIFIED="1445771758593" POSITION="left" TEXT="routing"/>
+<node COLOR="#0033ff" CREATED="1445772580312" FOLDED="true" ID="ID_602849438" MODIFIED="1445779498626" POSITION="left" TEXT="React">
+<node COLOR="#0033ff" CREATED="1445772583895" FOLDED="true" ID="ID_1159742150" MODIFIED="1445779498149" TEXT="render()">
+<node COLOR="#0033ff" CREATED="1445774371687" ID="ID_556294764" MODIFIED="1445774400941" TEXT="React.render(&lt;component&gt;, &lt;elemtn to insert into&gt;);"/>
+</node>
+</node>
+</node>
+</map>
